b24434bd49766aed079a0e4d8fdf9ee9bafb5e15
Alex Poms <alexpoms@gmail.com>
2016-09-15 20:09:36 -0400

    Add float type alias

--- scanner/util/common.h
+++ scanner/util/common.h
@@ -29,6 +29,8 @@ using u64 = uint64_t;
 using i8 = int8_t;
 using i32 = int32_t;
 using i64 = int64_t;
+using f32 = float;
+using f64 = double;
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Global constants
