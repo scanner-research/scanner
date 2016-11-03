0e64c61cefe915fc05674793defbbca97d109eb1
Will Crichton <wcrichto@stanford.edu>
2016-11-03 19:26:07 -0400

    Hopefully publishing docker

--- scripts/publish-docker.sh
+++ scripts/publish-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
 docker push $DOCKER_REPO:$DOCKER_TAG
