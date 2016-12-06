7b5331a4deeecf1d98fa654449cf552f05f7f435
Will Crichton <wcrichto@stanford.edu>
2016-12-06 01:20:34 -0500

    Added build infrastructure for Halide

---
+++ cmake/Modules/FindHalide.cmake
@@ -0,0 +1,36 @@
+# FindHalide.cmake
+# ... shamelessly based on FindJeMalloc.cmake
+
+
+set(HALIDE_ROOT_DIR "" CACHE PATH "Folder contains Halide")
+
+if (NOT "$ENV{Halide_DIR}" STREQUAL "")
+  set(HALIDE_ROOT_DIR $ENV{Halide_DIR})
+endif()
+
+find_library(HALIDE_LIBRARIES
+  NAMES Halide
+  HINTS ${HALIDE_ROOT_DIR}/lib
+  )
+
+find_path(HALIDE_INCLUDE_DIR
+  NAMES Halide.h HalideRuntime.h
+  HINTS ${HALIDE_ROOT_DIR}/include
+  )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Halide DEFAULT_MSG
+  HALIDE_LIBRARIES
+  HALIDE_INCLUDE_DIR
+  )
+
+set(HALIDE_LIBRARY HALIDE_LIBRARIES)
+set(HALIDE_INCLUDE_DIRS HALIDE_INCLUDE_DIR)
+
+mark_as_advanced(
+  HALIDE_ROOT_DIR
+  HALIDE_LIBRARY
+  HALIDE_LIBRARIES
+  HALIDE_INCLUDE_DIR
+  HALIDE_INCLUDE_DIRS
+  )
