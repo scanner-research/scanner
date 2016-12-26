d37c1403a2eae5bd8d552f6faa30725d8d27a68f
Alex Poms <alexpoms@gmail.com>
2016-12-26 17:03:43 -0500

    Working stereo rectification

--- python/jumpvr.py
+++ python/jumpvr.py
@@ -4,6 +4,7 @@ import numpy as np
 import struct
 import cv2
 import scipy.io as sio
+import scipy.misc
 import math
 
 
@@ -18,15 +19,15 @@ def load_calibration_data(path):
 
 
 def y_rotation_matrix(theta):
-    return np.matrix([[np.cos(theta), 0, -np.sin(theta)],
+    theta = theta - np.pi / 2
+    return np.matrix([[np.cos(theta), 0, np.sin(theta)],
                       [0, 1, 0],
-                      [np.sin(theta), 0, np.cos(theta)]])
+                      [-np.sin(theta), 0, np.cos(theta)]])
 
 def midpoint_transformation(calibration, cam_left_idx, cam_right_idx):
     num_cameras = calibration['numCameras']
     camera_radius = calibration['ringRadius']
     fov = calibration['fov'] * np.pi / 180.0
-    aspect_ratio = 1920.0 / 1080.0
 
     def radians_to_pos(rad):
         pos =  np.array([camera_radius * math.cos(rad),
@@ -49,100 +50,142 @@ def midpoint_transformation(calibration, cam_left_idx, cam_right_idx):
     cam_right_ext_inv = np.vstack((cam_right_ext_inv, np.array([0, 0, 0, 1])))
     cam_right_ext = np.linalg.inv(cam_right_ext_inv)
 
-    cam_mid_radians = (cam_left_radians + cam_right_radians) / 2
+    cam_mid_radians = cam_left_radians * 0.5 + cam_right_radians * 0.5
     cam_mid_pos = radians_to_pos(cam_mid_radians)
     cam_mid_rotation = y_rotation_matrix(cam_mid_radians)
     cam_mid_ext_inv = np.hstack((cam_mid_rotation, cam_mid_pos))
     cam_mid_ext_inv = np.vstack((cam_mid_ext_inv, np.array([0, 0, 0, 1])))
     cam_mid_ext = np.linalg.inv(cam_mid_ext_inv)
-    print(cam_left_radians, cam_right_radians, cam_mid_radians)
 
     print()
-    print(cam_left_ext_inv)
+    print('left',cam_left_ext_inv)
     print(cam_left_ext)
+    print('right',cam_right_ext_inv)
+    print(cam_right_ext)
+    print('mid', cam_mid_ext_inv)
     print(cam_mid_ext)
     # need to compute inverse of camera matrix and then apply mid point camera
 
     # transformation to get projection matrix
-    image = np.zeros((1080, 1920, 3), np.uint8)
-    # Fill image with red color(set each pixel to red)
-    image[:] = (255, 255, 255)
+    image = scipy.misc.imread('cam-1-frame-100.png')
+    imageLeft = image
+    imageRight = scipy.misc.imread('cam-2-frame-100.png')
 
-    cam_space = np.matrix([[1.0/(image.shape[0]/2), 0, -1.0],
-                           [0, 1.0/(image.shape[1]/2), -1.0],
+    width = image.shape[1]
+    height = image.shape[0]
+    aspect_ratio = width / (1.0*height)
+
+    cam_space = np.matrix([[1.0/(width/2), 0, -1.0],
+                           [0, 1.0/(height/2), -1.0],
                            [0, 0, 1]])
-    cam_space_inv = np.linalg.inv(cam_space)
+    cam_space_inv = np.matrix([[width/2, 0, 0, width/2],
+                               [0, height/2, 0, height/2],
+                               [0, 0, 1, 0],
+                               [0, 0, 0, 1]])
     cam_space = np.vstack((cam_space, np.array([0, 0, 1])))
     print(cam_space)
-    cam_space_inv = np.vstack((cam_space_inv, np.array([0, 0, 0])))
-    cam_space_inv = np.hstack((cam_space_inv, np.reshape(np.array([0, 0, 0, 1]), (4, 1))))
     print(cam_space_inv)
     ez = np.tan(fov / 2 )
-    perspective_inv = np.matrix([[ez, 0, 0, 0],
+    perspective_inv = np.matrix([[ez * aspect_ratio, 0, 0, 0],
                                  [0, ez, 0, 0],
                                  [0, 0, 1, 0],
                                  [0, 0, 0, 1]])
-    perspective = np.matrix([[1, 0, -1 / ez, 0],
-                             [0, 1, -1 / ez, 0],
+    perspective = np.matrix([[1 / (ez * aspect_ratio), 0, 0, 0],
+                             [0, 1 / ez, 0, 0],
                              [0, 0, 1, 0],
-                             [0, 0, 1 / ez, 0]])
+                             [0, 0, 1, 0]])
     #print('persp', np.linalg.inv(perspective))
 
-    left_to_mid_matrix = cam_mid_ext * cam_left_ext_inv * perspective_inv * cam_space
-    right_to_mid_matrix = cam_mid_ext * cam_right_ext_inv * perspective_inv * cam_space
+    left_to_mid_matrix1 = perspective_inv * cam_space
+    left_to_mid_matrix = perspective * cam_mid_ext * cam_left_ext_inv
+    right_to_mid_matrix1 = perspective_inv * cam_space
+    right_to_mid_matrix = perspective * cam_mid_ext * cam_right_ext_inv 
     print(left_to_mid_matrix)
     print(right_to_mid_matrix)
 
     # Create a blank 300x300 black image
 
-    out = np.zeros((1080, 1920, 3), np.uint8)
+    out = np.zeros((height, width, 3), np.uint8)
     out[:] = (0, 0, 0)
     source_points = np.matrix([[0.0, 0.0],
-                               [image.shape[1], 0],
-                               [0, image.shape[0]],
-                               [image.shape[1], image.shape[0]]], dtype=np.float32)
+                               [width, 0],
+                               [0, height],
+                               [width, height]], dtype=np.float32)
     left_destination_points = np.zeros((4, 2), dtype=np.float32)
     right_destination_points = np.zeros((4, 2), dtype=np.float32)
+    infz = 1000000000000.0
     for i in range(4):
         one = np.array([1])
         one.shape = (1, 1)
         p = np.hstack((source_points[i], one))
-        print(p)
-        zl = left_to_mid_matrix * p.T
-        print('zl', zl)
+        print('in',p)
+        zl = left_to_mid_matrix1 * p.T
+        print(zl)
+        zl[3] = 1/infz
+        zl *= 1/zl[3]
+        zl = left_to_mid_matrix * zl
+        print('ou',zl)
         zl /= zl[3]
-        zl = perspective * zl
-        zl /= zl[3]
-        zl[0] = zl[0] * image.shape[0] + image.shape[0]
-        zl[1] = zl[1] * image.shape[1] + image.shape[1]
-        zr = right_to_mid_matrix * p.T
+        zl[0] = zl[0] * width / 2 + width / 2
+        zl[1] = zl[1] * height / 2 + height / 2
+
+        zr = right_to_mid_matrix1 * p.T
+        zr[3] = 1/infz
+        zr *= 1/zr[3]
+        zr = right_to_mid_matrix * zr
         zr /= zr[3]
-        zr[0] = zr[0] * image.shape[0] + image.shape[0]
-        zr[1] = zr[1] * image.shape[1] + image.shape[1]
-        print('left', p, 'unpersp', perspective_inv * cam_space * p.T, 'center', zl)
+        zr[0] = zr[0] * width / 2 + width / 2
+        zr[1] = zr[1] * height / 2 + height / 2
+        print('p', p)
+        print('left', zl)
+        print('right', zr)
         left_destination_points[i,0] = zl[0]
         left_destination_points[i,1] = zl[1]
         right_destination_points[i,0] = zr[0]
         right_destination_points[i,1] = zr[1]
 
-    print(left_destination_points)
-    # destination_points = np.matrix([[], [], [], []])
+    print('dest pts', left_destination_points)
+    print('dest pts', right_destination_points)
+
+    zer1 = cam_mid_ext * cam_mid_ext_inv * perspective_inv * cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    zer2 = perspective_inv * cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    zer3 = perspective * cam_mid_ext * cam_mid_ext_inv * perspective_inv * cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    zer4 = cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    print('should be eq', zer1, zer2)
+    print('should be eq', zer3 / zer3[3], zer4)
+
+    mid_rot = np.vstack((np.hstack((cam_mid_rotation.T,
+                                    np.reshape(np.array([0, 0, 0]), (3, 1)))),
+                         np.array([0, 0, 0, 1])))
+    #zer5 = perspective * cam_mid_ext * cam_left_ext_inv * perspective_inv * cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    zer5 = cam_left_ext_inv * perspective_inv * cam_space * np.reshape(np.array([width, 0, 1]), (3, 1))
+    #zer5 = mid_rot * zer5
+    print('should be idk', zer5 )
+    zer6 = cam_mid_ext_inv * perspective_inv * cam_space * np.reshape(np.array([width/2, height/2, 1]), (3, 1))
+    #zer6 = mid_rot * zer6
+    print('should be idk', zer6 )
+    zer7 = cam_right_ext_inv * perspective_inv * cam_space * np.reshape(np.array([0, height, 1]), (3, 1))
+    #zer7 = mid_rot * zer7
+    print('should be idk', zer7 )
+    a5 = math.atan2(zer5[0], zer5[2]) * 180 / np.pi
+    a6 = math.atan2(zer6[0], zer6[2]) * 180 / np.pi
+    a7 = math.atan2(zer7[0], zer7[2]) * 180 / np.pi
+    print('angles', a5 - a6, a6 - a6, a7 - a6)
+    print('mid', perspective * cam_mid_ext * (zer5))
+    print('mid', perspective * mid_rot * (zer6))
+    print('mid', perspective * mid_rot * (zer7))
+    print('per', perspective)
+
     left_to_mid_warp = cv2.getPerspectiveTransform(source_points,
                                                    left_destination_points)
     right_to_mid_warp = cv2.getPerspectiveTransform(source_points,
                                                     right_destination_points)
-    right_to_mid_matrix = np.identity(3)
-    right_to_mid_matrix[0, 2] = 505
-    right_to_mid_matrix[1, 2] = 250 
-    right_to_mid_matrix[2, 0] = 0.0001
-    right_to_mid_matrix[2, 1] = 0.0001
-    #print(right_to_mid_matrix)
-    out = cv2.warpPerspective(image, left_to_mid_warp, (1920, 1080), out)
-    #print(image)
-    #print(out)
-    cv2.imwrite('warp.jpg', out)
+    out = cv2.warpPerspective(imageLeft, left_to_mid_warp, (width, height), out)
+    cv2.imwrite('warpLeft.jpg', out)
+    out = cv2.warpPerspective(imageRight, right_to_mid_warp, (width, height), out)
+    cv2.imwrite('warpRight.jpg', out)
 
 
 if __name__ == "__main__":
     calibration = load_calibration_data('calibration.json')
-    midpoint_transformation(calibration, 10, 11)
+    midpoint_transformation(calibration, 1, 2)
