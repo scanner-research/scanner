c361377d7c62ef24a80a89a98464a3dc25b9f4c7
Will Crichton <wcrichto@stanford.edu>
2017-02-15 15:10:54 -0500

    Re-enabled GPU build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 docker build -t $DOCKER_REPO:cpu . --build-arg gpu=OFF
 docker run $DOCKER_REPO:cpu /bin/bash -c "cd /opt/scanner/build && make test"
 docker rm $(docker ps -a -f status=exited -q)
-# docker build -t $DOCKER_REPO:gpu . --build-arg gpu=ON
+docker build -t $DOCKER_REPO:gpu . --build-arg gpu=ON
--- scripts/publish-docker.sh
+++ scripts/publish-docker.sh
@@ -2,4 +2,4 @@
 
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
 docker push $DOCKER_REPO:cpu
-# docker push $DOCKER_REPO:gpu
+docker push $DOCKER_REPO:gpu
