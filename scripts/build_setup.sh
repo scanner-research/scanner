0da0861782246d88dc6898363006adb9b47aa88c
Will Crichton <wcrichto@stanford.edu>
2017-02-15 02:57:04 -0500

    Attempted ssh key fix

--- scripts/build_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_519f11e8a6d4_key -iv $encrypted_519f11e8a6d4_iv -in .config/travisci_rsa.enc -out .config/travisci_rsa -d
-chmod 0600 .config/travisci_rsa
-eval `ssh-agent -s`
-ssh-add .config/travisci_rsa
--- scripts/publish-doxygen.sh
+++ scripts/publish-doxygen.sh
@@ -4,6 +4,13 @@ COMMIT_USER="Documentation Builder"
 COMMIT_EMAIL="wcrichto@cs.stanford.edu"
 CHANGESET=$(git rev-parse --verify HEAD)
 
+openssl aes-256-cbc -K $encrypted_519f11e8a6d4_key -iv $encrypted_519f11e8a6d4_iv -in .config/travisci_rsa.enc -out .config/travisci_rsa -d
+chmod 0600 .config/travisci_rsa
+eval `ssh-agent -s`
+ssh-add .config/travisci_rsa
+cp .config/travisci_rsa.pub ~/.ssh/id_rsa.pub
+
+
 rm -rf ${HTML_PATH}
 mkdir -p ${HTML_PATH}
 git clone -b gh-pages ${REPO_PATH} --single-branch ${HTML_PATH}
