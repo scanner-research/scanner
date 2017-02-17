d7a4e76b6cf25b789951fba559f0da384e7fdd4c
Will Crichton <wcrichto@stanford.edu>
2017-02-17 18:46:43 -0500

    Fixed CPU build

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -3,11 +3,14 @@
 # The Travis VM isn't big enough to hold two Docker images of Scanner,
 # so we have to push and delete the CPU image before building the GPU one.
 
-docker build -t $DOCKER_REPO:cpu . --build-arg gpu=OFF
-docker run $DOCKER_REPO:cpu /bin/bash -c "cd /opt/scanner/build && make test"
+build_docker() {
+    docker build -t $DOCKER_REPO:$1 . --build-arg gpu=$2
+    docker run $DOCKER_REPO:$1 /bin/bash -c "cd /opt/scanner/build && make test"
+    docker push $DOCKER_REPO:$1
+    docker rm $(docker ps -a -f status=exited -q)
+    docker rmi -f $DOCKER_REPO:$1
+}
+
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push $DOCKER_REPO:cpu
-docker rm $(docker ps -a -f status=exited -q)
-docker rmi -f $DOCKER_REPO:cpu
-docker build -t $DOCKER_REPO:gpu . --build-arg gpu=ON
-docker push $DOCKER_REPO:gpu
+build_docker cpu OFF
+build_docker gpu ON
