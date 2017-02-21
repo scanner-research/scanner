f0c80eaa90e5d2044589bb01c34332cb1cd7ad33
Alex Poms <alexpoms@gmail.com>
2017-02-21 13:27:38 -0500

    Always reset on IO item boundaries

--- scanner/engine/evaluate_worker.cpp
+++ scanner/engine/evaluate_worker.cpp
@@ -91,10 +91,11 @@ void *pre_evaluate_thread(void *arg) {
     auto work_start = now();
 
     bool needs_configure = !(io_item.table_id() == last_table_id);
-    bool needs_reset = needs_configure ||
-                       !(io_item.item_id() == last_item_id ||
-                         (io_item.table_id() == last_table_id &&
-                          io_item.start_row() == last_end_row));
+    bool needs_reset = true;
+    // NOTE(apoms): for avoiding warmup
+    // needs_configure || !(io_item.item_id() == last_item_id ||
+    //       (io_item.table_id() == last_table_id &&
+    //        io_item.start_row() == last_end_row));
 
     last_table_id = io_item.table_id();
     last_end_row = io_item.end_row();
