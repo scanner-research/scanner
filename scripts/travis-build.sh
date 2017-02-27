d8337e6c5b48021676b0afee2f24046097e4de1f
Will Crichton <wcrichto@stanford.edu>
2017-02-27 16:11:26 -0500

    Cleaned up scripts, consolidated tests

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -11,7 +11,7 @@ build_docker() {
     then
          docker build -t $DOCKER_REPO:$1-local . --build-arg gpu=OFF
          docker run $DOCKER_REPO:$1-local /bin/bash \
-                -c "cd /opt/scanner/build && make test && cd /opt/scanner && pytest tests -xv -m 'not gpu'"
+                -c "cd /opt/scanner/build && make test"
     else
          docker build -t $DOCKER_REPO:$1-local . --build-arg gpu=ON
     fi
@@ -21,6 +21,6 @@ build_docker() {
     docker rmi -f $DOCKER_REPO:$1
 }
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USER" -p="$DOCKER_PASS"
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 build_docker cpu
 build_docker gpu
