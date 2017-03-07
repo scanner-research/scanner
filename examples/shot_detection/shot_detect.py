7035f24e74f951a530810c94d7b905c9237450d3
Alex Poms <alexpoms@gmail.com>
2017-03-07 00:42:28 -0500

    Refactor shot_detection into functions

--- examples/shot_detection/shot_detect.py
+++ examples/shot_detection/shot_detect.py
@@ -18,29 +18,7 @@ except ImportError:
 
 WINDOW_SIZE = 500
 
-def main():
-    movie_path = util.download_video() if len(sys.argv) <= 1 else sys.argv[1]
-    print('Detecting shots in movie {}'.format(movie_path))
-
-    db = Database()
-    if not db.has_table('movie'):
-        print('Loading movie into Scanner database...')
-        db.ingest_videos([('movie', movie_path)], force=True)
-    movie_table = db.table('movie')
-
-    if not db.has_table('movie_hist'):
-        print('Computing a color histogram for each frame...')
-        db.run(
-            db.sampler().all([(movie_table.name(), 'movie_hist')]),
-            db.ops.Histogram(device=DeviceType.GPU),
-            force=True)
-    hists_table = db.table('movie_hist')
-
-    print('Computing shot boundaries...')
-
-    # Fetch histograms from disk
-    hists = [h for _, h in hists_table.load(['histogram'], parsers.histograms)]
-
+def compute_shot_boundaries(hists):
     # Compute the mean difference between each pair of adjacent frames
     diffs = np.array([np.mean([distance.chebyshev(hists[i-1][j], hists[i][j])
                                for j in range(3)])
@@ -58,33 +36,68 @@ def main():
         window = diffs[max(i-WINDOW_SIZE,0):min(i+WINDOW_SIZE,n)]
         if diffs[i] - np.mean(window) > 3 * np.std(window):
             boundaries.append(i)
+    return boundaries
 
-    print('Visualizing shot boundaries...')
 
-    # Loading the frames for each shot boundary
-    frames = [f[0] for _, f in
-              movie_table.load([0], rows=boundaries)]
-    n = len(frames)
-
-    (frame_h, frame_w, _) = frames[0].shape
+def make_montage(n, frames):
+    _, frame = frames.next()
+    frame = frame[0]
+    (frame_h, frame_w, _) = frame.shape
     target_w = 256
     target_h = int(target_w / float(frame_w) * frame_h)
     frames_per_row = 8
     img_w = frames_per_row * target_w
     img_h = int(math.ceil(float(n) / frames_per_row)) * target_h
-
     img = np.zeros((img_h, img_w, 3))
-    for i, frame in enumerate(frames):
-        frame = cv2.resize(frame, (target_w, target_h))
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    def place_image(i, fr):
+        fr = cv2.resize(fr, (target_w, target_h))
+        fr = cv2.cvtColor(fr, cv2.COLOR_RGB2BGR)
         row = i / frames_per_row
         col = i % frames_per_row
         img[(row * target_h):((row+1) * target_h),
             (col * target_w):((col+1) * target_w),
-            :] = frame
+            :] = fr
 
-    cv2.imwrite('shots.jpg', img)
-    print('Successfully generated shots.jpg')
+    place_image(0, frame)
+    for i, (_, frame) in enumerate(frames):
+        place_image(i + 1, frame[0])
+
+    return img
+
+
+def main():
+    movie_path = util.download_video() if len(sys.argv) <= 1 else sys.argv[1]
+    print('Detecting shots in movie {}'.format(movie_path))
+    movie_name = os.path.basename(movie_path)
+
+    with Database() as db:
+        if not db.has_table(movie_name):
+            print('Loading movie into Scanner database...')
+            db.ingest_videos([(movie_name, movie_path)], force=True)
+        movie_table = db.table(movie_name)
+
+        if not db.has_table(movie_name + '_hist'):
+            print('Computing a color histogram for each frame...')
+            db.run(
+                db.sampler().all([(movie_table.name(), movie_name + '_hist')]),
+                db.ops.Histogram(device=DeviceType.GPU),
+                force=True)
+        hists_table = db.table(movie_name + '_hist')
+
+        print('Computing shot boundaries...')
+        # Read histograms from disk
+        hists = [h for _, h in hists_table.load(['histogram'], parsers.histograms)]
+        boundaries = compute_shot_boundaries(hists)
+
+        print('Visualizing shot boundaries...')
+
+        # Loading the frames for each shot boundary
+        frames = movie_table.load([0], rows=boundaries)
+        montage_img = make_montage(len(boundaries), frames)
+
+        cv2.imwrite('shots.jpg', montage_img)
+        print('Successfully generated shots.jpg')
 
 if __name__ == "__main__":
     main()
