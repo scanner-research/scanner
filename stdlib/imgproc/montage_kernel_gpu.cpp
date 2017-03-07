138ddb9f28011aed9ddb6f268f962846a0c25912
Alex Poms <alexpoms@gmail.com>
2017-03-07 01:49:58 -0500

    Add kernel and python fn to make montage in scanner

---
+++ stdlib/imgproc/montage_kernel_gpu.cpp
@@ -0,0 +1,102 @@
+#include "scanner/api/op.h"
+#include "scanner/api/kernel.h"
+#include "scanner/util/cuda.h"
+#include "scanner/util/memory.h"
+#include "scanner/util/opencv.h"
+#include "stdlib/stdlib.pb.h"
+
+namespace scanner {
+
+class MontageKernelGPU : public VideoKernel {
+public:
+  MontageKernelGPU(const Kernel::Config &config)
+      : VideoKernel(config), device_(config.devices[0]),
+        frames_seen_(0) {
+    valid_.set_success(true);
+    if (!args_.ParseFromArray(config.args.data(), config.args.size())) {
+      RESULT_ERROR(&valid_, "MontageKernel could not parse protobuf args");
+      return;
+    }
+
+    num_frames_ = args_.num_frames();
+    target_width_ = args_.target_width();
+    frames_per_row_ = args_.frames_per_row();
+  }
+
+  void new_frame_info() override {
+    set_device();
+    frame_width_ = frame_info_.width();
+    frame_height_ = frame_info_.height();
+
+    target_height_ = (target_width_ / (1.0 * frame_width_) * frame_height_);
+
+    montage_width_ = frames_per_row_ * target_width_;
+    montage_height_ =
+        std::ceil(num_frames_ / (1.0 * frames_per_row_)) * target_height_;
+
+    montage_buffer_ =
+        new_buffer(device_, montage_width_ * montage_height_ * 3);
+    montage_image_ =
+        cvc::GpuMat(montage_height_, montage_width_, CV_8UC3, montage_buffer_);
+  }
+
+  void execute(const BatchedColumns &input_columns,
+               BatchedColumns &output_columns) override {
+    set_device();
+    check_frame_info(device_, input_columns[1]);
+
+    i32 input_count = input_columns[0].rows.size();
+    for (i32 i = 0; i < input_count; ++i) {
+      // TODO(wcrichto): implement correctly w/ streams
+      cvc::GpuMat img(frame_height_, frame_width_, CV_8UC3,
+                      input_columns[0].rows[i].buffer);
+      i64 x = frames_seen_ % frames_per_row_;
+      i64 y = frames_seen_ / frames_per_row_;
+      cvc::GpuMat montage_subimg =
+          montage_image_(cv::Rect(target_width_ * x, target_height_ * y,
+                                  target_width_, target_height_));
+      cvc::resize(img, montage_subimg, cv::Size(target_width_, target_height_));
+
+      frames_seen_++;
+      if (frames_seen_ == num_frames_) {
+        output_columns[0].rows.push_back(
+            Row{montage_buffer_, montage_width_ * montage_height_ * 3});
+      } else {
+        output_columns[0].rows.push_back(Row{new_buffer(device_, 1), 1});
+      }
+    }
+  }
+
+  void set_device() {
+    CUDA_PROTECT({ CU_CHECK(cudaSetDevice(device_.id)); });
+    cvc::setDevice(device_.id);
+  }
+
+private:
+  proto::Result valid_;
+  DeviceHandle device_;
+  proto::MontageArgs args_;
+  i64 num_frames_ = args_.num_frames();
+  i32 frame_width_;
+  i32 frame_height_;
+  i32 target_width_;
+  i32 target_height_;
+  i32 frames_per_row_;
+
+  i64 montage_width_;
+  i64 montage_height_;
+
+  u8 *montage_buffer_;
+  cvc::GpuMat montage_image_;
+  i64 frames_seen_;
+};
+
+REGISTER_OP(Montage)
+    .inputs({"frame", "frame_info"})
+    .outputs({"montage"});
+
+
+REGISTER_KERNEL(Montage, MontageKernelGPU)
+    .device(DeviceType::GPU)
+    .num_devices(1);
+}
