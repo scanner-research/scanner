ea742e6560edb6a6c3cbccfbbb9ba75eda1e5ae9
Documentation Builder <wcrichto@cs.stanford.edu>
2017-03-10 06:17:26 +0000

    Automated documentation build for changeset 69527aadfb45259c272e16ddf85fa2328a8c373b.

--- search/all_f.js
+++ search/all_f.js
@@ -5,7 +5,5 @@ var searchData=
   ['scannerexception',['ScannerException',['../classscannerpy_1_1common_1_1ScannerException.html',1,'scannerpy::common']]],
   ['serverstate',['ServerState',['../structscanner_1_1Database_1_1ServerState.html',1,'scanner::Database']]],
   ['set_5fprofiler',['set_profiler',['../classscanner_1_1Kernel.html#abd94a7909c3eca198e04460c2891272f',1,'scanner::Kernel']]],
-  ['start_5fcluster',['start_cluster',['../classscannerpy_1_1database_1_1Database.html#a2ad31e38ecaf4beb29a2a3342be15d07',1,'scannerpy::database::Database']]],
-  ['start_5fmaster',['start_master',['../classscannerpy_1_1database_1_1Database.html#a33031617f4621326882e5ea9f1968521',1,'scannerpy::database::Database']]],
-  ['start_5fworker',['start_worker',['../classscannerpy_1_1database_1_1Database.html#adc376903dac1a43e9952467834b3ff4a',1,'scannerpy::database::Database']]]
+  ['start_5fcluster',['start_cluster',['../classscannerpy_1_1database_1_1Database.html#a2ad31e38ecaf4beb29a2a3342be15d07',1,'scannerpy::database::Database']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['set_5fprofiler',['set_profiler',['../classscanner_1_1Kernel.html#abd94a7909c3eca198e04460c2891272f',1,'scanner::Kernel']]],
-  ['start_5fcluster',['start_cluster',['../classscannerpy_1_1database_1_1Database.html#a2ad31e38ecaf4beb29a2a3342be15d07',1,'scannerpy::database::Database']]],
-  ['start_5fmaster',['start_master',['../classscannerpy_1_1database_1_1Database.html#a33031617f4621326882e5ea9f1968521',1,'scannerpy::database::Database']]],
-  ['start_5fworker',['start_worker',['../classscannerpy_1_1database_1_1Database.html#adc376903dac1a43e9952467834b3ff4a',1,'scannerpy::database::Database']]]
+  ['start_5fcluster',['start_cluster',['../classscannerpy_1_1database_1_1Database.html#a2ad31e38ecaf4beb29a2a3342be15d07',1,'scannerpy::database::Database']]]
 ];
