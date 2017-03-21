33337e79ec467786f65172d04521f43d8093602e
Alex Poms <alexpoms@gmail.com>
2017-03-21 14:08:45 -0400

    Misc fixes

--- scanner/engine/halide_context.cpp
+++ scanner/engine/halide_context.cpp
@@ -1,11 +1,5 @@
 #include "scanner/util/cuda.h"
 
-namespace Halide {
-namespace Runtime {
-namespace Internal {
-namespace Cuda {
+namespace Halide { namespace Runtime { namespace Internal { namespace Cuda {
 CUcontext context = 0;
-}
-}
-}
-}
+}}}}
--- scanner/engine/halide_context.h
+++ scanner/engine/halide_context.h
@@ -1,10 +1,12 @@
 #include "scanner/util/cuda.h"
 
+#pragma once
+
 namespace Halide {
 namespace Runtime {
 namespace Internal {
 namespace Cuda {
-CUcontext context = 0;
+extern CUcontext context = 0;
 }
 }
 }
