7c46d39ac12cac4b4ede1462bac0e29007ce090e
Documentation Builder <wcrichto@cs.stanford.edu>
2017-03-29 15:43:10 +0000

    Automated documentation build for changeset 90aa5404496e685ebefd5e9392a4f55bf57ee6cd.

--- search/all_b.js
+++ search/all_b.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['netdescriptor',['NetDescriptor',['../classscannerpy_1_1stdlib_1_1net__descriptor_1_1NetDescriptor.html',1,'scannerpy::stdlib::net_descriptor']]],
   ['new_5fcollection',['new_collection',['../classscannerpy_1_1database_1_1Database.html#aebc9f5b93594a1ad4cc9e948f56cfc47',1,'scannerpy::database::Database']]],
-  ['new_5fframe_5finfo',['new_frame_info',['../classscanner_1_1VideoKernel.html#a4bae7eb944b6099db1ff94db1eed616d',1,'scanner::VideoKernel']]]
+  ['new_5fframe_5finfo',['new_frame_info',['../classscanner_1_1VideoKernel.html#a4bae7eb944b6099db1ff94db1eed616d',1,'scanner::VideoKernel']]],
+  ['new_5ftable',['new_table',['../classscannerpy_1_1database_1_1Database.html#aee220a63e8a452d1646230948a46f949',1,'scannerpy::database::Database']]]
 ];
