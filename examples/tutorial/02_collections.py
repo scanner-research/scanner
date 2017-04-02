5c5b99d5c8a3a990c57d7a0f9f3abdf89d7098d3
John Emmons <emmons.john@gmail.com>
2017-04-02 14:41:19 -0400

    fix abspath for python example 02

--- examples/tutorial/02_collections.py
+++ examples/tutorial/02_collections.py
@@ -1,7 +1,7 @@
 from scannerpy import Database
 import sys
 import os.path
-sys.path.append(os.path.dirname(__file__) + '/..')
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 import util
 
 ################################################################################
