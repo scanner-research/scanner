04b410a0784d5a8e6546d329017fb8ca84418a97
Will Crichton <wcrichto@cs.stanford.edu>
2017-04-14 16:46:12 -0400

    Updated OpenFace kernel

---
+++ python/scannerpy/stdlib/writers.py
@@ -0,0 +1,9 @@
+import struct
+
+def bboxes(bufs):
+    s = struct.pack('=Q', len(bufs[0]))
+    for bbox in bufs[0]:
+        bs = bbox.SerializeToString()
+        s += struct.pack('=i', len(bs))
+        s += bs
+    return [s]
