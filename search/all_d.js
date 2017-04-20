e966fcfbf5b25b7f034af37739a879d2f616ff20
Documentation Builder <wcrichto@cs.stanford.edu>
2017-04-20 22:35:18 +0000

    Automated documentation build for changeset fe8082e86cd41f9886358e488201bbaa01424949.

--- search/all_d.js
+++ search/all_d.js
@@ -5,5 +5,6 @@ var searchData=
   ['opcolumn',['OpColumn',['../classscannerpy_1_1op_1_1OpColumn.html',1,'scannerpy::op']]],
   ['opgenerator',['OpGenerator',['../classscannerpy_1_1op_1_1OpGenerator.html',1,'scannerpy::op']]],
   ['opinput',['OpInput',['../classscanner_1_1OpInput.html',1,'scanner']]],
-  ['ops',['ops',['../classscannerpy_1_1database_1_1Database.html#a77f1b14450c7cbd30607ef268e6b6a6c',1,'scannerpy::database::Database']]]
+  ['ops',['ops',['../classscannerpy_1_1database_1_1Database.html#a77f1b14450c7cbd30607ef268e6b6a6c',1,'scannerpy::database::Database']]],
+  ['outputcolumncompression',['OutputColumnCompression',['../structscanner_1_1OutputColumnCompression.html',1,'scanner']]]
 ];
