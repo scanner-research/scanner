a21ae699b1a92be6b7d49714ed59bfe41900f510
Alex Poms <alexpoms@gmail.com>
2017-04-20 03:35:59 -0400

    Don't expose protobuf messages in headers
    Protobuf messages from different libraries do not work well together.
    Specifically, the strings inside protobuf messages were all getting aliased
    to a single value. This was occuring because there were two "empty strings",
    one from libscanner and one from libstdlib, that were conflicting and causing
    the default string to be overwrriten.

--- scanner/api/op.cpp
+++ scanner/api/op.cpp
@@ -55,8 +55,24 @@ namespace internal {
 OpRegistration::OpRegistration(const OpBuilder& builder) {
   const std::string& name = builder.name_;
   const bool variadic_inputs = builder.variadic_inputs_;
-  const std::vector<Column>& input_columns = builder.input_columns_;
-  const std::vector<Column>& output_columns = builder.output_columns_;
+  std::vector<Column> input_columns;
+  size_t i = 0;
+  for (auto& name_type : builder.input_columns_) {
+    Column col;
+    col.set_id(i++);
+    col.set_name(std::get<0>(name_type));
+    col.set_type(std::get<1>(name_type));
+    input_columns.push_back(col);
+  }
+  std::vector<Column> output_columns;
+  i = 0;
+  for (auto& name_type : builder.output_columns_) {
+    Column col;
+    col.set_id(i++);
+    col.set_name(std::get<0>(name_type));
+    col.set_type(std::get<1>(name_type));
+    output_columns.push_back(col);
+  }
   OpInfo* info =
     new OpInfo(name, variadic_inputs, input_columns, output_columns);
   OpRegistry* registry = get_op_registry();
