64dd86cb09d3d17d8775b7e8a97b14e79ac8a687
Alex Poms <alexpoms@gmail.com>
2017-04-26 01:31:10 -0400

    Add get_caffe_facenet script, fixup cpm2 get script

--- nets/get_cpm2.sh
+++ nets/get_cpm2.sh
@@ -7,18 +7,10 @@ prog() {
     mkdir nets/cpm2
     cd nets/cpm2
 
-    # Prototxt for COCO version
-    wget https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/caffe_demo/master/model/coco/pose_deploy_linevec.prototxt
-    mv pose_deploy_linvec.prototxt coco_pose_deploy_linvec.prototxt
     # Prototxt for MPI version
-    wget https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/caffe_demo/master/model/mpi/pose_deploy_linevec.prototxt
-    mv pose_deploy_linvec.prototxt mpi_pose_deploy_linvec.prototxt
-    # Caffemodel for COCO
-    wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/coco/pose_iter_440000.caffemodel
-    mv pose_iter_440000.caffemodel coco_pose_iter_440000.caffemodel
+    wget https://storage.googleapis.com/scanner-data/nets/cpm2/mpi_pose_deploy_linevec.prototxt
     # Caffemodel for MPI
-    wget http://posefs1.perception.cs.cmu.edu/Users/tsimon/Projects/coco/data/models/mpi/pose_iter_160000.caffemodel
-    mv pose_iter_160000.caffemodel mpi_pose_iter_160000.caffemodel
+    wget https://storage.googleapis.com/scanner-data/nets/cpm2/mpi_pose_iter_160000.caffemodel
 
     cd $CWD
 }
