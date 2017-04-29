0e78e8bb103435b12a8df4acdec5630c21ad7a37
Will Crichton <wcrichto@cs.stanford.edu>
2017-04-29 01:20:50 -0400

    Fixed GPU resize

--- stdlib/imgproc/resize_kernel.cpp
+++ stdlib/imgproc/resize_kernel.cpp
@@ -1,21 +1,24 @@
 #include "scanner/api/kernel.h"
 #include "scanner/api/op.h"
+#include "scanner/util/cuda.h"
 #include "scanner/util/memory.h"
 #include "scanner/util/opencv.h"
 #include "stdlib/stdlib.pb.h"
 
 namespace scanner {
 
-class ResizeKernelCPU : public VideoKernel {
+class ResizeKernel : public VideoKernel {
  public:
-  ResizeKernelCPU(const Kernel::Config& config) : VideoKernel(config) {
+  ResizeKernel(const Kernel::Config& config)
+    : VideoKernel(config), device_(config.devices[0]) {
     args_.ParseFromArray(config.args.data(), config.args.size());
   }
 
   void execute(const BatchedColumns& input_columns,
                BatchedColumns& output_columns) override {
     auto& frame_col = input_columns[0];
-    check_frame(CPU_DEVICE, frame_col[0]);
+    set_device();
+    check_frame(device_, frame_col[0]);
 
     i32 target_width = args_.width();
     i32 target_height = args_.height();
@@ -38,23 +41,39 @@ class ResizeKernelCPU : public VideoKernel {
 
     i32 input_count = num_rows(frame_col);
     FrameInfo info(target_height, target_width, 3, FrameType::U8);
-    std::vector<Frame*> output_frames =
-        new_frames(CPU_DEVICE, info, input_count);
+    std::vector<Frame*> output_frames = new_frames(device_, info, input_count);
 
     for (i32 i = 0; i < input_count; ++i) {
-      cv::Mat img = frame_to_mat(frame_col[i].as_const_frame());
-      cv::Mat out_mat = frame_to_mat(output_frames[i]);
-
-      cv::resize(img, out_mat, cv::Size(target_width, target_height));
+      if (device_.type == DeviceType::CPU) {
+        cv::Mat img = frame_to_mat(frame_col[i].as_const_frame());
+        cv::Mat out_mat = frame_to_mat(output_frames[i]);
+        cv::resize(img, out_mat, cv::Size(target_width, target_height));
+      } else {
+        cvc::GpuMat img = frame_to_gpu_mat(frame_col[i].as_const_frame());
+        cvc::GpuMat out_mat = frame_to_gpu_mat(output_frames[i]);
+        cvc::resize(img, out_mat, cv::Size(target_width, target_height));
+      }
       insert_frame(output_columns[0], output_frames[i]);
     }
   }
 
+  void set_device() {
+    if (device_.type == DeviceType::GPU) {
+      CUDA_PROTECT({ CU_CHECK(cudaSetDevice(device_.id)); });
+      cvc::setDevice(device_.id);
+    }
+  }
+
  private:
+  DeviceHandle device_;
   proto::ResizeArgs args_;
 };
 
 REGISTER_OP(Resize).frame_input("frame").frame_output("frame");
 
-REGISTER_KERNEL(Resize, ResizeKernelCPU).device(DeviceType::CPU).num_devices(1);
+REGISTER_KERNEL(Resize, ResizeKernel).device(DeviceType::CPU).num_devices(1);
+
+#ifdef HAVE_CUDA
+REGISTER_KERNEL(Resize, ResizeKernel).device(DeviceType::GPU).num_devices(1);
+#endif
 }
