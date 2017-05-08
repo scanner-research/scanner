cfffc2931ff71dcdc3992ce13821e510aad4eb9f
Will Crichton <wcrichto@cs.stanford.edu>
2017-05-08 00:06:29 -0400

    Fixed type size in bbox writer

--- python/scannerpy/stdlib/writers.py
+++ python/scannerpy/stdlib/writers.py
@@ -4,6 +4,6 @@ def bboxes(bufs):
     s = struct.pack('=Q', len(bufs[0]))
     for bbox in bufs[0]:
         bs = bbox.SerializeToString()
-        s += struct.pack('=i', len(bs))
+        s += struct.pack('=Q', len(bs))
         s += bs
     return [s]
