4c013ce10e60717ab56bc8d41607ca25ee57a558
Documentation Builder <wcrichto@cs.stanford.edu>
2017-07-30 05:09:22 +0000

    Automated documentation build for changeset b86aacbd5af7b1965eab07883c81dc5fb74b4582.

--- search/classes_c.js
+++ search/classes_c.js
@@ -4,5 +4,6 @@ var searchData=
   ['tablesample',['TableSample',['../structscanner_1_1TableSample.html',1,'scanner']]],
   ['tablesampler',['TableSampler',['../classscannerpy_1_1sampler_1_1TableSampler.html',1,'scannerpy::sampler']]],
   ['task',['Task',['../structscanner_1_1Task.html',1,'scanner']]],
-  ['taskset',['TaskSet',['../structscanner_1_1TaskSet.html',1,'scanner']]]
+  ['taskset',['TaskSet',['../structscanner_1_1TaskSet.html',1,'scanner']]],
+  ['tensorflowkernel',['TensorFlowKernel',['../classscannerpy_1_1stdlib_1_1pykernel_1_1TensorFlowKernel.html',1,'scannerpy::stdlib::pykernel']]]
 ];
