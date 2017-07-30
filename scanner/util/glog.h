07f30041675da4c4a2c6409e544f025ded8a6a77
Alex Poms <alexpoms@gmail.com>
2017-07-30 19:36:17 -0400

    Add proper glog logging

---
+++ scanner/util/glog.h
@@ -0,0 +1,28 @@
+/* Copyright 2017 Carnegie Mellon University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "glog/logging.h"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace scanner {
+
+void init_glog(char* program_name);
+
+}
