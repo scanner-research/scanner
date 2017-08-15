94c98dd5900d6b464a0494ec0c4816972734444f
Documentation Builder <wcrichto@cs.stanford.edu>
2017-08-15 01:23:05 +0000

    Automated documentation build for changeset d7549432d20db0755f1f8ec40895ece833b205c0.

--- search/variables_4.js
+++ search/variables_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['profiler_5f',['profiler_',['../classscanner_1_1BaseKernel.html#ad01a16f12b895ccc3375f82ccd391876',1,'scanner::BaseKernel::profiler_()'],['../classscanner_1_1StenciledBatchedKernel.html#ad333246fadfa61752c9a536278b460b8',1,'scanner::StenciledBatchedKernel::profiler_()']]],
-  ['protobufs',['protobufs',['../classscannerpy_1_1database_1_1Database.html#a1ff4ae03fba6d1cf13886a6adad120d3',1,'scannerpy::database::Database']]]
+  ['ops',['ops',['../classscannerpy_1_1database_1_1Database.html#a77f1b14450c7cbd30607ef268e6b6a6c',1,'scannerpy::database::Database']]]
 ];
