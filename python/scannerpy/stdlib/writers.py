b9d93b13a1fc842c5cabf0b6ef493f6c0e239158
Alex Poms <alexpoms@gmail.com>
2017-08-16 03:02:03 -0400

    Fix writing to the db with new_table

--- python/scannerpy/stdlib/writers.py
+++ python/scannerpy/stdlib/writers.py
@@ -1,9 +1,24 @@
 import struct
 
-def bboxes(bufs):
+def bboxes(bufs, db):
     s = struct.pack('=Q', len(bufs[0]))
     for bbox in bufs[0]:
         bs = bbox.SerializeToString()
         s += struct.pack('=Q', len(bs))
         s += bs
+    return [s]
+
+def poses(bufs, db):
+    s = struct.pack("=Q", len(bufs[0]))
+    for pose in bufs[0]:
+        # Num joints
+        s += struct.pack("=Q", len(pose))
+        for i in range(len(pose)):
+            point = db.protobufs.Point()
+            point.y = pose[i, 0]
+            point.x = pose[i, 1]
+            point.score = pose[i, 2]
+            # Point size
+            s += struct.pack("=Q", point.ByteSize())
+            s += point.SerializeToString()
     return [s]
