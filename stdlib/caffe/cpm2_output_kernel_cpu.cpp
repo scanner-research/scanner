ff4843175379a6dbc01e9bf37a83d2b45e3b2e53
Alex Poms <alexpoms@gmail.com>
2017-08-16 03:00:03 -0400

    Change cpm2 to use coco keypoints instead of MPI

--- stdlib/caffe/cpm2_output_kernel_cpu.cpp
+++ stdlib/caffe/cpm2_output_kernel_cpu.cpp
@@ -118,7 +118,7 @@ class CPM2OutputKernel : public BatchedKernel, public VideoKernel {
     proto::CPM2Args args;
     args.ParseFromArray(config.args.data(), config.args.size());
     scale_ = args.scale();
-    modeldesc.reset(new MPIModelDescriptor());
+    modeldesc.reset(new COCOModelDescriptor());
 
     joints_.resize(max_people_ * 3 * max_num_parts_);
   }
@@ -137,7 +137,7 @@ class CPM2OutputKernel : public BatchedKernel, public VideoKernel {
 
     feature_width_ = net_input_width_;
     feature_height_ = net_input_height_;
-    feature_channels_ = 44;
+    feature_channels_ = 57;
   }
 
   void execute(const BatchedColumns& input_columns,
@@ -164,8 +164,10 @@ class CPM2OutputKernel : public BatchedKernel, public VideoKernel {
 
       std::vector<std::vector<double>> subset;
       std::vector<std::vector<std::vector<double>>> connection;
-      int count =
-          connect_limbs(subset, connection, heatmap, peaks, joints_.data());
+      // int count =
+      //     connect_limbs(subset, connection, heatmap, peaks, joints_.data());
+      int count = connect_limbs_coco(subset, connection, heatmap, peaks,
+                                     joints_.data());
 
       std::vector<std::vector<scanner::Point>> bodies(count);
       for (int p = 0; p < count; ++p) {
@@ -458,6 +460,299 @@ class CPM2OutputKernel : public BatchedKernel, public VideoKernel {
     return cnt;
   }
 
+  int connect_limbs_coco(
+      std::vector<std::vector<double>>& subset,
+      std::vector<std::vector<std::vector<double>>>& connection,
+      const float* heatmap_pointer, const float* peaks,
+      float* joints) {
+    /* Parts Connection ---------------------------------------*/
+    const auto num_parts = modeldesc->num_parts();
+    const auto limbSeq = modeldesc->get_limb_seq();
+    const auto mapIdx = modeldesc->get_map_idx();
+    const auto number_limb_seq = modeldesc->num_limb_seq();
+
+    CHECK_EQ(num_parts, 18) << "Wrong connection function for model";
+    CHECK_EQ(number_limb_seq, 19) << "Wrong connection function for model";
+
+    int SUBSET_CNT = num_parts + 2;
+    int SUBSET_SCORE = num_parts + 1;
+    int SUBSET_SIZE = num_parts + 3;
+
+    const int peaks_offset = 3 * (max_peaks_ + 1);
+
+    subset.clear();
+    connection.clear();
+
+    for (int k = 0; k < number_limb_seq; k++) {
+      const float* map_x =
+          heatmap_pointer +
+          mapIdx[2 * k] * net_input_height_ * net_input_width_;
+      const float* map_y =
+          heatmap_pointer +
+          mapIdx[2 * k + 1] * net_input_height_ * net_input_width_;
+
+      const float* candA = peaks + limbSeq[2 * k] * peaks_offset;
+      const float* candB = peaks + limbSeq[2 * k + 1] * peaks_offset;
+
+      std::vector<std::vector<double>> connection_k;
+      int nA = candA[0];
+      int nB = candB[0];
+
+      // add parts into the subset in special case
+      if (nA == 0 && nB == 0) {
+        continue;
+      } else if (nA == 0) {
+        for (int i = 1; i <= nB; i++) {
+          int num = 0;
+          int indexB = limbSeq[2 * k + 1];
+          for (int j = 0; j < subset.size(); j++) {
+            int off = limbSeq[2 * k + 1] * peaks_offset + i * 3 + 2;
+            if (subset[j][indexB] == off) {
+              num = num + 1;
+              continue;
+            }
+          }
+          if (num != 0) {
+            // LOG(INFO) << " else if (nA==0) shouldn't have any nB already
+            // assigned?";
+          } else {
+            std::vector<double> row_vec(SUBSET_SIZE, 0);
+            row_vec[limbSeq[2 * k + 1]] = limbSeq[2 * k + 1] * peaks_offset +
+                                          i * 3 + 2;  // store the index
+            row_vec[SUBSET_CNT] = 1;  // last number in each row is the parts
+                                      // number of that person
+            row_vec[SUBSET_SCORE] =
+                candB[i * 3 +
+                      2];  // second last number in each row is the total score
+            subset.push_back(row_vec);
+          }
+          // LOG(INFO) << "nA==0 New subset on part " << k << " subsets: " <<
+          // subset.size();
+        }
+        continue;
+      } else if (nB == 0) {
+        for (int i = 1; i <= nA; i++) {
+          int num = 0;
+          int indexA = limbSeq[2 * k];
+          for (int j = 0; j < subset.size(); j++) {
+            int off = limbSeq[2 * k] * peaks_offset + i * 3 + 2;
+            if (subset[j][indexA] == off) {
+              num = num + 1;
+              continue;
+            }
+          }
+          if (num == 0) {
+            std::vector<double> row_vec(SUBSET_SIZE, 0);
+            row_vec[limbSeq[2 * k]] =
+                limbSeq[2 * k] * peaks_offset + i * 3 + 2;  // store the index
+            row_vec[SUBSET_CNT] = 1;  // last number in each row is the parts
+                                      // number of that person
+            row_vec[SUBSET_SCORE] =
+                candA[i * 3 +
+                      2];  // second last number in each row is the total score
+            subset.push_back(row_vec);
+            // LOG(INFO) << "nB==0 New subset on part " << k << " subsets: " <<
+            // subset.size();
+          } else {
+            // LOG(INFO) << "nB==0 discarded would have added";
+          }
+        }
+        continue;
+      }
+
+      std::vector<std::vector<double>> temp;
+      const int num_inter = 10;
+
+      for (int i = 1; i <= nA; i++) {
+        for (int j = 1; j <= nB; j++) {
+          float s_x = candA[i * 3];
+          float s_y = candA[i * 3 + 1];
+          float d_x = candB[j * 3] - candA[i * 3];
+          float d_y = candB[j * 3 + 1] - candA[i * 3 + 1];
+          float norm_vec = sqrt(d_x * d_x + d_y * d_y);
+          if (norm_vec < 1e-6) {
+            // The peaks are coincident. Don't connect them.
+            continue;
+          }
+          float vec_x = d_x / norm_vec;
+          float vec_y = d_y / norm_vec;
+
+          float sum = 0;
+          int count = 0;
+
+          for (int lm = 0; lm < num_inter; lm++) {
+            int my = round(s_y + lm * d_y / num_inter);
+            int mx = round(s_x + lm * d_x / num_inter);
+            if (mx >= net_input_width_) {
+              // LOG(ERROR) << "mx " << mx << "out of range";
+              mx = net_input_width_ - 1;
+            }
+            if (my >= net_input_height_) {
+              // LOG(ERROR) << "my " << my << "out of range";
+              my = net_input_height_ - 1;
+            }
+            CHECK_GE(mx, 0);
+            CHECK_GE(my, 0);
+            int idx = my * net_input_width_ + mx;
+            float score = (vec_x * map_x[idx] + vec_y * map_y[idx]);
+            if (score > connect_inter_threshold_) {
+              sum = sum + score;
+              count++;
+            }
+          }
+          // float score = sum / count; // + std::min((130/dist-1),0.f)
+
+          if (count >
+              connect_inter_min_above_threshold_) {  // num_inter*0.8) {
+                                                           // //thre/2
+            // parts score + cpnnection score
+            std::vector<double> row_vec(4, 0);
+            row_vec[3] =
+                sum / count + candA[i * 3 + 2] + candB[j * 3 + 2];  // score_all
+            row_vec[2] = sum / count;
+            row_vec[0] = i;
+            row_vec[1] = j;
+            temp.push_back(row_vec);
+          }
+        }
+      }
+
+      //** select the top num connection, assuming that each part occur only
+      //once
+      // sort rows in descending order based on parts + connection score
+      if (temp.size() > 0) std::sort(temp.begin(), temp.end(), ColumnCompare());
+
+      int num = std::min(nA, nB);
+      int cnt = 0;
+      std::vector<int> occurA(nA, 0);
+      std::vector<int> occurB(nB, 0);
+
+      for (int row = 0; row < temp.size(); row++) {
+        if (cnt == num) {
+          break;
+        } else {
+          int i = int(temp[row][0]);
+          int j = int(temp[row][1]);
+          float score = temp[row][2];
+          if (occurA[i - 1] == 0 && occurB[j - 1] == 0) {  // && score> (1+thre)
+            std::vector<double> row_vec(3, 0);
+            row_vec[0] = limbSeq[2 * k] * peaks_offset + i * 3 + 2;
+            row_vec[1] = limbSeq[2 * k + 1] * peaks_offset + j * 3 + 2;
+            row_vec[2] = score;
+            connection_k.push_back(row_vec);
+            cnt = cnt + 1;
+            occurA[i - 1] = 1;
+            occurB[j - 1] = 1;
+          }
+        }
+      }
+
+      //** cluster all the joints candidates into subset based on the part
+      //connection
+      // initialize first body part connection 15&16
+      if (k == 0) {
+        std::vector<double> row_vec(num_parts + 3, 0);
+        for (int i = 0; i < connection_k.size(); i++) {
+          double indexB = connection_k[i][1];
+          double indexA = connection_k[i][0];
+          row_vec[limbSeq[0]] = indexA;
+          row_vec[limbSeq[1]] = indexB;
+          row_vec[SUBSET_CNT] = 2;
+          // add the score of parts and the connection
+          row_vec[SUBSET_SCORE] =
+              peaks[int(indexA)] + peaks[int(indexB)] + connection_k[i][2];
+          // LOG(INFO) << "New subset on part " << k << " subsets: " <<
+          // subset.size();
+          subset.push_back(row_vec);
+        }
+      } /* else if (k==17 || k==18) { // TODO: Check k numbers?
+                //   %add 15 16 connection
+                for(int i = 0; i < connection_k.size(); i++) {
+                    double indexA = connection_k[i][0];
+                    double indexB = connection_k[i][1];
+
+                    for(int j = 0; j < subset.size(); j++) {
+                    // if subset(j, indexA) == partA(i) && subset(j, indexB) == 0
+                    //         subset(j, indexB) = partB(i);
+                    // elseif subset(j, indexB) == partB(i) && subset(j, indexA) == 0
+                    //         subset(j, indexA) = partA(i);
+                    // end
+                        if (subset[j][limbSeq[2*k]] == indexA && subset[j][limbSeq[2*k+1]]==0) {
+                            subset[j][limbSeq[2*k+1]] = indexB;
+                        } else if (subset[j][limbSeq[2*k+1]] == indexB && subset[j][limbSeq[2*k]]==0) {
+                            subset[j][limbSeq[2*k]] = indexA;
+                        }
+                }
+                continue;
+            }
+        }*/ else {
+        if (connection_k.size() == 0) {
+          continue;
+        }
+
+        // A is already in the subset, find its connection B
+        for (int i = 0; i < connection_k.size(); i++) {
+          int num = 0;
+          double indexA = connection_k[i][0];
+          double indexB = connection_k[i][1];
+
+          for (int j = 0; j < subset.size(); j++) {
+            if (subset[j][limbSeq[2 * k]] == indexA) {
+              subset[j][limbSeq[2 * k + 1]] = indexB;
+              num = num + 1;
+              subset[j][SUBSET_CNT] = subset[j][SUBSET_CNT] + 1;
+              subset[j][SUBSET_SCORE] = subset[j][SUBSET_SCORE] +
+                                        peaks[int(indexB)] + connection_k[i][2];
+            }
+          }
+          // if can not find partA in the subset, create a new subset
+          if (num == 0) {
+            // LOG(INFO) << "New subset on part " << k << " subsets: " <<
+            // subset.size();
+            std::vector<double> row_vec(SUBSET_SIZE, 0);
+            row_vec[limbSeq[2 * k]] = indexA;
+            row_vec[limbSeq[2 * k + 1]] = indexB;
+            row_vec[SUBSET_CNT] = 2;
+            row_vec[SUBSET_SCORE] =
+                peaks[int(indexA)] + peaks[int(indexB)] + connection_k[i][2];
+            subset.push_back(row_vec);
+          }
+        }
+      }
+    }
+
+    //** joints by deleteing some rows of subset which has few parts occur
+    int cnt = 0;
+    for (int i = 0; i < subset.size(); i++) {
+      if (subset[i][SUBSET_CNT] < 1) {
+        LOG(INFO) << "BAD SUBSET_CNT";
+      }
+      if (subset[i][SUBSET_CNT] >= connect_min_subset_cnt_ &&
+          (subset[i][SUBSET_SCORE] / subset[i][SUBSET_CNT]) >
+              connect_min_subset_score_) {
+        for (int j = 0; j < num_parts; j++) {
+          int idx = int(subset[i][j]);
+          if (idx) {
+            joints[cnt * num_parts * 3 + j * 3 + 2] = peaks[idx];
+            joints[cnt * num_parts * 3 + j * 3 + 1] = peaks[idx - 1] *
+                                                      frame_info_.shape[0] /
+                                                      (float)net_input_height_;
+            joints[cnt * num_parts * 3 + j * 3] =
+                peaks[idx - 2] * frame_info_.shape[1] / (float)net_input_width_;
+          } else {
+            joints[cnt * num_parts * 3 + j * 3 + 2] = 0;
+            joints[cnt * num_parts * 3 + j * 3 + 1] = 0;
+            joints[cnt * num_parts * 3 + j * 3] = 0;
+          }
+        }
+        cnt++;
+        if (cnt == max_people_) break;
+      }
+    }
+
+    return cnt;
+  }
+
  private:
   f32 threshold_ = 0.5f;
 
@@ -479,12 +774,21 @@ class CPM2OutputKernel : public BatchedKernel, public VideoKernel {
 
   const int max_people_ = 96;
   const int max_num_parts_ = 70;
-  const int max_peaks_ = 20;
-  const int num_joints_ = 15;
-  int connect_min_subset_cnt_ = 3;
+  // For MPI
+  //const int max_peaks_ = 64;
+  // For COCO
+  const int max_peaks_ = 64;
+  const int num_joints_ = 18;
+  // For MPI
+  /*int connect_min_subset_cnt_ = 3;
   float connect_min_subset_score_ = 0.4;
   float connect_inter_threshold_ = 0.01;
-  int connect_inter_min_above_threshold_ = 8;
+  int connect_inter_min_above_threshold_ = 8;*/
+  // For COCO
+  int connect_min_subset_cnt_ = 3;
+  float connect_min_subset_score_ = 0.4;
+  float connect_inter_threshold_ = 0.050;
+  int connect_inter_min_above_threshold_ = 9;
   std::vector<float> joints_;
 };
 
