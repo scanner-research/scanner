4f0fd33348f0db5aad349f41420c8a1708030bbc
Documentation Builder <wcrichto@cs.stanford.edu>
2017-08-23 21:16:14 +0000

    Automated documentation build for changeset 3c64fc14551484696c0b86b50374ae3b762cfe5c.

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['basekernel',['BaseKernel',['../classscanner_1_1BaseKernel.html',1,'scanner']]],
-  ['batchedkernel',['BatchedKernel',['../classscanner_1_1BatchedKernel.html',1,'scanner']]]
+  ['batchedkernel',['BatchedKernel',['../classscanner_1_1BatchedKernel.html',1,'scanner']]],
+  ['bboxnmskernel',['BBoxNMSKernel',['../classscannerpy_1_1stdlib_1_1bbox__nms__kernel_1_1BBoxNMSKernel.html',1,'scannerpy::stdlib::bbox_nms_kernel']]]
 ];
--- search/all_a.js
+++ search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['kernel',['Kernel',['../classscanner_1_1Kernel.html',1,'scanner']]],
+  ['kernel',['Kernel',['../classscannerpy_1_1kernel_1_1Kernel.html',1,'scannerpy::kernel']]],
   ['kernelconfig',['KernelConfig',['../structscanner_1_1KernelConfig.html',1,'scanner']]]
 ];
