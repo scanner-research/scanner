07931262230fc6857050bf1a35655a32922a4dc1
Alex Poms <alexpoms@gmail.com>
2017-10-05 00:08:51 -0400

    WIP finish master + start on worker

--- scanner/engine/op_info.h
+++ scanner/engine/op_info.h
@@ -28,13 +28,17 @@ class OpInfo {
   OpInfo(const std::string& name, bool variadic_inputs,
          const std::vector<Column>& input_columns,
          const std::vector<Column>& output_columns, bool can_stencil,
-         const std::vector<i32> preferred_stencil)
+         const std::vector<i32> preferred_stencil, bool bounded_state,
+         i32 warmup, bool unbounded_state)
     : name_(name),
       variadic_inputs_(variadic_inputs),
       input_columns_(input_columns),
       output_columns_(output_columns),
       can_stencil_(can_stencil),
-      preferred_stencil_(preferred_stencil) {}
+      preferred_stencil_(preferred_stencil),
+      bounded_state_(bounded_state),
+      warmup_(warmup),
+      unbounded_state_(unbounded_state) {}
 
   const std::string& name() const { return name_; }
 
@@ -50,6 +54,14 @@ class OpInfo {
     return preferred_stencil_;
   }
 
+  const bool has_bounded_state() const { return bounded_state_; }
+
+  const i32 warmup() const {
+    return warmup_;
+  }
+
+  const bool has_unbounded_state() const { return unbounded_state_; }
+
  private:
   std::string name_;
   bool variadic_inputs_;
@@ -57,6 +69,9 @@ class OpInfo {
   std::vector<Column> output_columns_;
   bool can_stencil_;
   std::vector<i32> preferred_stencil_;
+  bool bounded_state_;
+  i32 warmup_;
+  bool unbounded_state_;
 };
 }
 }
