6027388f55c05ce23904a719765b08e9a2c05a0b
Alex Poms <alexpoms@gmail.com>
2017-10-28 13:56:01 -0400

    Update pipeline file, add resnet example

---
+++ examples/caffe/resnet.py
@@ -0,0 +1,42 @@
+from scannerpy import Database, DeviceType, Job, BulkJob
+from scannerpy.stdlib import NetDescriptor
+import numpy as np
+import cv2
+import struct
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+import util
+
+video_path = util.download_video() if len(sys.argv) <= 1 else sys.argv[1]
+print('Performing classification on video {}'.format(video_path))
+video_name = os.path.splitext(os.path.basename(video_path))[0]
+
+with Database() as db:
+    [input_table], _ = db.ingest_videos(
+        [(video_name, video_path)], force=True)
+
+    descriptor = NetDescriptor.from_file(db, 'nets/resnet.toml')
+
+    batch_size = 48
+    frame = db.ops.FrameInput()
+    caffe_input = db.ops.CaffeInput(
+        frame = frame,
+        net_descriptor = descriptor.as_proto(),
+        batch_size = batch_size,
+        device=DeviceType.GPU)
+    caffe_output = db.ops.Caffe(
+        caffe_frame = caffe_input,
+        net_descriptor = descriptor.as_proto(),
+        batch_size = batch_size,
+        batch = batch_size,
+        device=DeviceType.GPU)
+    output = db.ops.Output(columns=[caffe_output])
+
+    job = Job(op_args={
+        frame: input_table.column('frame'),
+        output: input_table.name() + '_classification'
+    })
+    bulk_job = BulkJob(output=output, jobs=[job])
+
+    [output] = db.run(bulk_job, pipeline_instances_per_node=1, force=True)
