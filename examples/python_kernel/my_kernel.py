243f11876a078f876ad8070ea18ed33931eb41cf
Qian Li <liqian.cs@gmail.com>
2017-11-08 17:32:16 -0800

    update

--- examples/python_kernel/my_kernel.py
+++ examples/python_kernel/my_kernel.py
@@ -1,14 +1,37 @@
 import scannerpy
 import struct
+import numpy as np
+from PIL import Image
+import StringIO
+import time
+import sys
+import cv2
 
 class MyOpKernel(scannerpy.Kernel):
-    def __init__(self, config, protobufs):
-        self.protobufs = protobufs
+  def __init__(self, config, protobufs):
+    self.protobufs = protobufs
 
-    def close(self):
-        pass
+  def close(self):
+    pass
 
-    def execute(self, input_columns):
-        return [struct.pack('=q', 9000)]
+  def encode(arr, format="jpeg"):
+    im = Image.fromarray(arr)
+    buf = StringIO.StringIO()
+    im.save(buf, format=format)
+    output = buf.getvalue()
+    buf.close()
+    return output
+
+  def execute(self, input_columns):
+    # cv2_im = cv2.cvtColor(input_columns[0],cv2.COLOR_BGR2RGB)
+    pil_im = Image.fromarray(input_columns[0])
+    width, height = pil_im.size
+    print('width {}, height {}'.format(width, height))
+    # print arr
+    # arr = input_columns[0]
+    # jpeg_image = encode(arr)
+    # print('Raw={:d}, Jpeg={:d}'.format(len(encode(arr, format="bmp")), len(jpeg_image)))
+      # print('list size :{:d}'.format(len(input_columns[0])))
+    return [struct.pack('=q', 9000)]
 
 KERNEL = MyOpKernel
