8f40074b1b8545c15a35dbc89a2b9a81b0769068
Will Crichton <wcrichto@stanford.edu>
2017-11-24 22:14:23 -0500

    Attempt to fix login/push on branch updates

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e
+
 # The Travis VM isn't big enough to hold two Docker images of Scanner,
 # so we have to push and delete the CPU image before building the GPU one.
 
-set -e
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    PUSH=0
+else
+    PUSH=1
+fi
 
 build_docker() {
     # We add -local to make sure it doesn't run the remote image if the build fails.
@@ -16,11 +22,18 @@ build_docker() {
     else
          docker build -t $DOCKER_REPO:$1-local . --build-arg gpu=ON
     fi
-    docker tag $DOCKER_REPO:$1-local $DOCKER_REPO:$1
-    docker push $DOCKER_REPO:$1
+
+    if [ $PUSH -eq 0 ]; then
+        docker tag $DOCKER_REPO:$1-local $DOCKER_REPO:$1
+        docker push $DOCKER_REPO:$1
+    fi
+
     docker rmi -f $DOCKER_REPO:$1
 }
 
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+if [ $PUSH -eq 0 ]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+fi
+
 build_docker cpu
 build_docker gpu
