1d67c6d9f454e4425149876309c56c44d2ad5f72
Documentation Builder <wcrichto@cs.stanford.edu>
2017-11-30 06:25:21 +0000

    Automated documentation build for changeset b16f6139d8f67464e8ff8050e61152356782d4f9.

--- search/classes_2.js
+++ search/classes_2.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['database',['Database',['../classscanner_1_1Database.html',1,'scanner']]],
   ['database',['Database',['../classpython_1_1scannerpy_1_1database_1_1Database.html',1,'python::scannerpy::database']]],
+  ['devicehandle',['DeviceHandle',['../classpython_1_1scannerpy_1_1common_1_1DeviceHandle.html',1,'python::scannerpy::common']]],
   ['devicetype',['DeviceType',['../classpython_1_1scannerpy_1_1common_1_1DeviceType.html',1,'python::scannerpy::common']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['kernel',['Kernel',['../classscanner_1_1Kernel.html',1,'scanner']]],
   ['kernel',['Kernel',['../classpython_1_1scannerpy_1_1kernel_1_1Kernel.html',1,'python::scannerpy::kernel']]],
-  ['kernelconfig',['KernelConfig',['../structscanner_1_1KernelConfig.html',1,'scanner']]]
+  ['kernel',['Kernel',['../classscanner_1_1Kernel.html',1,'scanner']]],
+  ['kernelconfig',['KernelConfig',['../structscanner_1_1KernelConfig.html',1,'scanner']]],
+  ['kernelconfig',['KernelConfig',['../classpython_1_1scannerpy_1_1kernel_1_1KernelConfig.html',1,'python::scannerpy::kernel']]]
 ];
