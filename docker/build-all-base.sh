6b358f2419b36b785017ae87f389ca89c84d2a6c
Alex Poms <alexpoms@gmail.com>
2017-12-18 23:28:43 -0500

    Update docker files to build properly

--- docker/build-all-base.sh
+++ docker/build-all-base.sh
@@ -3,16 +3,20 @@
 NO_CACHE=false
 CORES=16
 
-for dir in ./*/
-do
-    dir=`basename ${dir%*/}`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-    base_tag=scannerresearch/scanner-base:$dir
+for dir in $DIR/*/
+do
+    base=`basename ${dir%*/}`
+
+    cp $DIR/../deps.sh $dir/deps.sh
+
+    base_tag=scannerresearch/scanner-base:$base
     docker build --build-arg cores=$CORES \
            --no-cache=$NO_CACHE -t $base_tag \
            -f $dir/Dockerfile.base $dir
 
-    tag=scannerresearch/scanner-base:$dir-gpu
+    tag=scannerresearch/scanner-base:$base-gpu
     docker build \
            --build-arg base_tag=$base_tag \
            --build-arg cores=$CORES \
@@ -20,7 +24,7 @@ do
            -f $dir/Dockerfile.gpu $dir
     docker push $tag
 
-    tag=scannerresearch/scanner-base:$dir-cpu
+    tag=scannerresearch/scanner-base:$base-cpu
     docker build \
            --build-arg base_tag=$base_tag \
            --build-arg cores=$CORES \
