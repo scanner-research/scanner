ed0ab781ed84812ad75890189fd397e579597225
Will Crichton <wcrichto@stanford.edu>
2017-12-31 16:18:53 -0500

    travis_wait doesn't exist?

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -20,7 +20,7 @@ build_docker() {
                -f docker/Dockerfile.scanner
         # travis_wait allows tests to run for N minutes with no output
         # https://docs.travis-ci.com/user/common-build-problems/#Build-times-out-because-no-output-was-received
-        travis_wait 30 docker run $DOCKER_REPO:$1-local /bin/bash \
+        docker run $DOCKER_REPO:$1-local /bin/bash \
                -c "cd /opt/scanner/build && CTEST_OUTPUT_ON_FAILURE=1 make test"
         docker rm $(docker ps -a -f status=exited -q)
     else
