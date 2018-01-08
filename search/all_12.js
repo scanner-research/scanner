8eb48b626df35f3ca9206180ca795c5ec3653c67
Documentation Builder <wcrichto@cs.stanford.edu>
2018-01-08 20:28:19 +0000

    Automated documentation build for changeset 28f4d2a58bd867a1ade47215753ace426e029d2c.

--- search/all_12.js
+++ search/all_12.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['table',['Table',['../classscannerpy_1_1table_1_1Table.html',1,'scannerpy::table']]],
   ['taskpartitioner',['TaskPartitioner',['../classscannerpy_1_1partitioner_1_1TaskPartitioner.html',1,'scannerpy::partitioner']]],
-  ['tensorflowkernel',['TensorFlowKernel',['../classscannerpy_1_1stdlib_1_1pykernel_1_1TensorFlowKernel.html',1,'scannerpy::stdlib::pykernel']]]
+  ['tensorflowkernel',['TensorFlowKernel',['../classscannerpy_1_1stdlib_1_1kernel_1_1TensorFlowKernel.html',1,'scannerpy::stdlib::kernel']]]
 ];
