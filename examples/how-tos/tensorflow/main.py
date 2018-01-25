409ba6db2f0917f562b9da2f5aa98b0ddf2da0c9
Ted Shaowang <tedswjz@gmail.com>
2018-01-25 13:24:46 -0800

    Download the DNN model if not found in PATH_TO_GRAPH

--- examples/how-tos/tensorflow/main.py
+++ examples/how-tos/tensorflow/main.py
@@ -1,12 +1,35 @@
 from scannerpy import Database, Job, BulkJob, ColumnType, DeviceType
 import os
 import sys
+import six.moves.urllib as urllib
+import tarfile
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_REPO = script_dir
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_GRAPH = os.path.join(PATH_TO_REPO, 'ssd_mobilenet_v1_coco_2017_11_17', 'frozen_inference_graph.pb')
 
 if len(sys.argv) <= 1:
     print('Usage: {:s} path/to/your/video/file.mp4'.format(sys.argv[0]))
     sys.exit(1)
+
+# Download the DNN model if not found in PATH_TO_GRAPH
+if not os.path.isfile(PATH_TO_GRAPH):
+    print("DNN Model not found, now downloading...")
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+        file_name = os.path.basename(file.name)
+        if 'frozen_inference_graph.pb' in file_name:
+            tar_file.extract(file, os.getcwd())
+    print("Successfully downloaded DNN Model.")
 
 movie_path = sys.argv[1]
 print('Detecting objects in movie {}'.format(movie_path))
