ee53fec57da7f39fbd37d5fec06b0441f66595ea
Alex Poms <alexpoms@gmail.com>
2018-04-08 12:32:18 -0700

    Support multiple encoding types for ImageEncoder

--- stdlib/imgproc/image_encoder_kernel_cpu.cpp
+++ stdlib/imgproc/image_encoder_kernel_cpu.cpp
@@ -2,12 +2,26 @@
 #include "scanner/api/op.h"
 #include "scanner/util/memory.h"
 #include "scanner/util/opencv.h"
+#include "stdlib/stdlib.pb.h"
 
 namespace scanner {
 
 class ImageEncoderKernel : public BatchedKernel, public VideoKernel {
  public:
-  ImageEncoderKernel(const KernelConfig& config) : BatchedKernel(config) {}
+  ImageEncoderKernel(const KernelConfig& config) : BatchedKernel(config) {
+    proto::ImageEncoderArgs args;
+    bool parsed = args.ParseFromArray(config.args.data(), config.args.size());
+    if (!parsed) {
+      RESULT_ERROR(&valid_, "Could not parse ImageEncoderArgs");
+      return;
+    }
+    image_type_ = args.format();
+  }
+
+  void validate(proto::Result* result) {
+    result->set_msg(valid_.msg());
+    result->set_success(valid_.success());
+  }
 
   void execute(const BatchedElements& input_columns,
                BatchedElements& output_columns) override {
@@ -15,8 +29,11 @@ class ImageEncoderKernel : public BatchedKernel, public VideoKernel {
     check_frame(CPU_DEVICE, frame_col[0]);
 
     std::vector<i32> encode_params;
-    encode_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-    encode_params.push_back(100);
+    if (image_type_ == "jpg" || image_type_ == "jpeg" || image_type_ == "JPG" ||
+        image_type_ == "jpeg") {
+      encode_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+      encode_params.push_back(100);
+    }
 
     i32 input_count = num_rows(frame_col);
     for (i32 i = 0; i < input_count; ++i) {
@@ -24,13 +41,17 @@ class ImageEncoderKernel : public BatchedKernel, public VideoKernel {
       std::vector<u8> buf;
       cv::Mat recolored;
       cv::cvtColor(img, recolored, CV_RGB2BGR);
-      bool success = cv::imencode(".jpg", recolored, buf, encode_params);
+      bool success = cv::imencode("." + image_type_, recolored, buf, encode_params);
       LOG_IF(FATAL, !success) << "Failed to encode image";
       u8* output_buf = new_buffer(CPU_DEVICE, buf.size());
       std::memcpy(output_buf, buf.data(), buf.size());
       insert_element(output_columns[0], output_buf, buf.size());
     }
   }
+
+ private:
+  proto::Result valid_;
+  std::string image_type_;
 };
 
 REGISTER_OP(ImageEncoder).frame_input("frame").output("img");
