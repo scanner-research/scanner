d85d03109b30599127bc3f4d9e8b0bd687aab245
Alex Poms <alexpoms@gmail.com>
2018-04-22 00:21:35 -0700

    Update README with use cases and add paper citation ref

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import sphinx_readable_theme
 sys.path.insert(0, os.path.abspath('../build/pip'))
 
 
@@ -91,7 +92,8 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
+html_theme = 'readable'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
