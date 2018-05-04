0e51efa9cf476c0a77d332fc1f83f37d37532cbf
Will Crichton <wcrichto@cs.stanford.edu>
2018-05-04 18:56:12 +0000

    Initial SQL source implementation

--- python/scannerpy/protobuf_generator.py
+++ python/scannerpy/protobuf_generator.py
@@ -1,4 +1,3 @@
-
 import os.path
 import imp
 import sys
@@ -16,11 +15,14 @@ import scanner.engine.rpc_pb2_grpc as grpc_types
 import scanner.types_pb2 as misc_types
 from google.protobuf.descriptor import FieldDescriptor
 
+
 class ProtobufGenerator:
     def __init__(self, cfg):
         self._mods = []
-        for mod in [misc_types, rpc_types, grpc_types, metadata_types,
-                    source_types, sink_types, sampler_types, stdlib_types]:
+        for mod in [
+                misc_types, rpc_types, grpc_types, metadata_types,
+                source_types, sink_types, sampler_types, stdlib_types
+        ]:
             self.add_module(mod)
 
     def add_module(self, path):
@@ -43,54 +45,64 @@ class ProtobufGenerator:
 
 
 def python_to_proto(protos, proto_name, obj):
-    args_proto = getattr(protos, proto_name)()
+    args_proto = getattr(protos, proto_name)
 
     def analyze_proto(p):
         fields = {}
-        for f in p.DESCRIPTOR.fields:
+        for f in p.fields:
             child_fields = None
             if f.type == FieldDescriptor.TYPE_MESSAGE:
                 child_fields = analyze_proto(f.message_type)
 
             fields[f.name] = {
-                'type': f.type,
-                'message': f.message_type,
-                'repeated': f.label == FieldDescriptor.LABEL_REPEATED,
-                'fields': child_fields,
+                'type':
+                f.type,
+                'message':
+                getattr(protos, f.message_type.name)
+                if f.message_type is not None else None,
+                'repeated':
+                f.label == FieldDescriptor.LABEL_REPEATED,
+                'fields':
+                child_fields,
             }
         return fields
 
-    p = analyze_proto(args_proto)
+    p = analyze_proto(args_proto.DESCRIPTOR)
 
-    def serialize_obj(args_proto, p, obj):
-        if isinstance(obj, dict):
-            for k, v in obj.items():
-                if k not in p:
-                    raise ScannerException(
-                        'Protobuf does not have field {:s}'.format(k))
-                desc = p[k]
-                # If a message field
-                def make_field(val):
-                    if desc['type'] == FieldDescriptor.TYPE_MESSAGE:
-                        # If a message field, we need to recursively invoke
-                        # serialization
-                        return serialize_obj(
-                            desc['message'], desc['fields'], val)
-                    else:
-                        return val
-                if p[k]['repeated']:
-                    # If a repeated field, we need to set using slicing
-                    data = []
-                    for vi in v:
-                        data.append(make_field(vi))
-                    getattr(args_proto, k)[:] = data
-                else:
-                    # Just set the regular field
-                    setattr(args_proto, k, make_field(v))
-        else:
+    def create_obj(proto, p, obj):
+        proto_obj = proto()
+        if not isinstance(obj, dict):
             raise ScannerException('Attempted to bind a non-dict type to a '
                                    'protobuf')
 
-    serialize_obj(args_proto, p, obj)
+        for k, v in obj.items():
+            if k not in p:
+                raise ScannerException(
+                    'Protobuf does not have field {:s}'.format(k))
+            desc = p[k]
 
-    return args_proto.SerializeToString()
+            # If a message field
+            def make_field(val):
+                if desc['type'] == FieldDescriptor.TYPE_MESSAGE:
+                    # If a message field, we need to recursively invoke
+                    # serialization
+                    return create_obj(desc['message'], desc['fields'], val)
+                else:
+                    return val
+
+            if p[k]['repeated']:
+                # If a repeated field, we need to set using slicing
+                data = []
+                for vi in v:
+                    data.append(make_field(vi))
+                getattr(proto_obj, k)[:] = data
+            elif p[k]['message'] is not None:
+                # If a message field, have to CopyFrom, can't use direct assignment
+                getattr(proto_obj, k).CopyFrom(make_field(v))
+            else:
+                # Just set the regular field
+                setattr(proto_obj, k, make_field(v))
+
+        return proto_obj
+
+    return create_obj(args_proto, p, obj).SerializeToString()
---
+++ stdlib/misc/sql_source.cpp
@@ -0,0 +1,208 @@
+/* Copyright 2018 Carnegie Mellon University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "scanner/api/source.h"
+#include "scanner/api/enumerator.h"
+#include "stdlib/stdlib.pb.h"
+
+#include "storehouse/storage_backend.h"
+#include "scanner/engine/video_index_entry.h"
+#include "scanner/engine/table_meta_cache.h"
+#include "scanner/util/tinyformat.h"
+#include "scanner/util/serialize.h"
+
+#include <glog/logging.h>
+#include <vector>
+#include <pqxx/pqxx>
+
+using storehouse::StorageBackend;
+using storehouse::StorageConfig;
+using storehouse::StoreResult;
+using storehouse::WriteFile;
+using storehouse::RandomReadFile;
+
+namespace scanner {
+
+// NOTE: SQLSource/Enumerator currently only supports Postgres.
+
+class SQLEnumerator : public Enumerator {
+ public:
+  SQLEnumerator(const EnumeratorConfig& config) : Enumerator(config), conn_(nullptr) {
+    bool parsed = args_.ParseFromArray(config.args.data(), config.args.size());
+    if (!parsed) {
+      RESULT_ERROR(&valid_, "Could not parse SQLEnumeratorArgs");
+      return;
+    }
+
+    // Setup connection to database
+    auto sql_config = args_.config();
+    conn_.reset(new pqxx::connection{
+      tfm::format("hostaddr=%s port=%d dbname=%s user=%s password=%s",
+                  sql_config.hostaddr(), sql_config.port(), sql_config.dbname(), sql_config.user(), sql_config.password())
+    });
+  }
+
+  i64 total_elements() override {
+    pqxx::work txn{*conn_};
+    // Count the number the number of groups
+    pqxx::row r = txn.exec1(tfm::format(
+        "SELECT COUNT(DISTINCT(%s)) FROM %s WHERE %s",
+        args_.query().group(), args_.query().table(), args_.filter()));
+    return r[0].as<i32>();
+  }
+
+  ElementArgs element_args_at(i64 element_idx) override {
+    proto::SQLElementArgs args;
+    args.set_filter(args_.filter());
+    size_t size = args.ByteSizeLong();
+
+    ElementArgs element_args;
+    element_args.args.resize(size);
+    args.SerializeToArray(element_args.args.data(), size);
+    element_args.row_id = element_idx;
+
+    return element_args;
+  }
+
+ private:
+  Result valid_;
+  scanner::proto::SQLEnumeratorArgs args_;
+  std::unique_ptr<pqxx::connection> conn_;
+};
+
+class SQLSource : public Source {
+ public:
+  SQLSource(const SourceConfig& config) :
+      Source(config), conn_(nullptr) {
+    bool parsed = args_.ParseFromArray(config.args.data(), config.args.size());
+    if (!parsed) {
+      RESULT_ERROR(&valid_, "Could not parse SQLSourceArgs");
+      return;
+    }
+
+    // Setup connection to database
+    auto sql_config = args_.config();
+    conn_.reset(new pqxx::connection{
+      tfm::format("hostaddr=%s port=%d dbname=%s user=%s password=%s",
+                  sql_config.hostaddr(), sql_config.port(), sql_config.dbname(), sql_config.user(), sql_config.password())
+    });
+  }
+
+  void read(const std::vector<ElementArgs>& element_args,
+            std::vector<Elements>& output_columns) override {
+    LOG_IF(FATAL, element_args.size() == 0) << "Asked to read zero elements";
+
+    // Deserialize all ElementArgs
+    std::string filter;
+    std::vector<i64> row_ids;
+    for (size_t i = 0; i < element_args.size(); ++i) {
+      proto::SQLElementArgs a;
+      bool parsed = a.ParseFromArray(element_args[i].args.data(),
+                                     element_args[i].args.size());
+      LOG_IF(FATAL, !parsed) << "Could not parse element args in SQL";
+
+      row_ids.push_back(element_args[i].row_id);
+      filter = a.filter();
+    }
+
+    auto query = args_.query();
+
+    // If we received elements from a new job, then flush our cached results and run a new query
+    if (last_filter_ != filter) {
+      last_filter_ = filter;
+
+      // Execute SELECT to fetch all the rows
+      pqxx::work txn{*conn_};
+      std::string query_str = tfm::format(
+          "SELECT %s, %s AS number FROM %s WHERE %s ORDER BY number",
+          query.fields(), query.group(), query.table(), filter);
+
+      pqxx::result result = txn.exec(query_str);
+      LOG_IF(FATAL, result.size() == 0) << "Query returned zero rows. Executed query was:\n" << query_str;
+
+      // Group the rows based on the provided key
+      cached_rows_.clear();
+      std::vector<pqxx::row> cur_group;
+      i32 last_group = -1;
+      for (auto row : result) {
+        i32 num = row["number"].as<i32>();
+        if (num != last_group) {
+          last_group = num;
+          cached_rows_.push_back(cur_group);
+          cur_group = std::vector<pqxx::row>();
+        }
+        cur_group.push_back(row);
+      }
+    }
+
+    size_t total_size = 0;
+    std::vector<size_t> sizes;
+    std::vector<u8*> buffers;
+
+    // Pick a serialization method based on the requested output type
+    if (query.output_type() == "BoundingBox") {
+      std::vector<std::vector<proto::BoundingBox> > bboxes;
+      for (auto row_id : row_ids) {
+        bboxes.emplace_back();
+        auto& row_bboxes = bboxes[bboxes.size()-1];
+        for (auto row : cached_rows_[row_id]) {
+          row_bboxes.emplace_back();
+          auto& bb = row_bboxes[row_bboxes.size() - 1];
+          bb.set_x1(row["x1"].as<f32>());
+          bb.set_y1(row["y1"].as<f32>());
+          bb.set_x2(row["x2"].as<f32>());
+          bb.set_y2(row["y2"].as<f32>());
+        }
+      }
+
+      for (auto& bb : bboxes) {
+        u8* buffer;
+        size_t size;
+        serialize_proto_vector(bb, buffer, size);
+        sizes.push_back(size);
+        buffers.push_back(buffer);
+        total_size += size;
+      }
+    } else {
+      LOG(FATAL) << "Invalid query output type " << query.output_type();
+    }
+
+    // Pack serialized results into a single block buffer;
+    u8* block_buffer = new_block_buffer(CPU_DEVICE, total_size, buffers.size());
+    u8* cursor = block_buffer;
+    for (i32 i = 0; i < buffers.size(); ++i) {
+      memcpy_buffer(cursor, CPU_DEVICE, buffers[i], CPU_DEVICE, sizes[i]);
+      delete_buffer(CPU_DEVICE, buffers[i]);
+      insert_element(output_columns[0], cursor, sizes[i]);
+      cursor += sizes[i];
+    }
+  }
+
+ private:
+  Result valid_;
+  std::unique_ptr<pqxx::connection> conn_;
+  scanner::proto::SQLSourceArgs args_;
+  std::string last_filter_;
+  std::vector<std::vector<pqxx::row>> cached_rows_;
+};
+
+REGISTER_ENUMERATOR(SQL, SQLEnumerator)
+    .protobuf_name("SQLEnumeratorArgs");
+
+REGISTER_SOURCE(SQL, SQLSource)
+    .output("output")
+    .protobuf_name("SQLSourceArgs");
+
+}  // namespace scanner
