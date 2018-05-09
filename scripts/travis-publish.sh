3bdd3c6d0a5d507077a0762b102cbb76fa9ac1d2
Alex Poms <alexpoms@gmail.com>
2018-05-09 10:34:46 -0700

    More travis debug

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -17,6 +17,8 @@ CHANGESET=$(git rev-parse --verify HEAD)
 # Install python package for autodoc
 docker run $DOCKER_REPO:cpu-local /bin/bash -c "
 cd /opt/scanner
+git config --global user.name \"${COMMIT_USER}\"
+git config --global user.email \"${COMMIT_EMAIL}\"
 
 # Unencrypt ssh key
 mkdir -p ~/.ssh/
@@ -24,17 +26,18 @@ chmod 0700 ~/.ssh/
 
 openssl aes-256-cbc -K $encrypted_519f11e8a6d4_key -iv $encrypted_519f11e8a6d4_iv -in .travis/travisci_rsa.enc -out .travis/travisci_rsa -d
 chmod 0600 .travis/travisci_rsa
-eval `ssh-agent -s`
-ls .travis
-ls ~/.ssh/
-ssh-add .travis/travisci_rsa
+cp .travis/travisci_rsa ~/.ssh/id_rsa
 cp .travis/travisci_rsa.pub ~/.ssh/id_rsa.pub
 chmod 0744 ~/.ssh/id_rsa.pub
+ls -lah .travis
+ls -lah ~/.ssh/
+
+eval `ssh-agent -s`
+ssh-add
 
 pip3 install doxypypy twine
 pip3 install Sphinx sphinx_readable_theme sphinx-autodoc-typehints
 
-bash build.sh
 rm -rf ${HTML_PATH}
 mkdir -p ${HTML_PATH}
 git clone -b gh-pages ${REPO_PATH} --single-branch ${HTML_PATH}
@@ -51,8 +54,6 @@ cd -
 cd ${HTML_PATH}
 cp /tmp/CNAME .
 git add .
-git config --global user.name \"${COMMIT_USER}\"
-git config --global user.email \"${COMMIT_EMAIL}\"
 git commit -m \"Automated documentation build for changeset ${CHANGESET}.\"
 git push origin gh-pages
 cd -
