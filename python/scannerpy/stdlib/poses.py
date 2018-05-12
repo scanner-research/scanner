fe039ba8b7e62459d4ac14318ef63e57436267ac
Will Crichton <wcrichto@cs.stanford.edu>
2018-05-12 22:02:37 +0000

    Add drawing to pose class

--- python/scannerpy/stdlib/poses.py
+++ python/scannerpy/stdlib/poses.py
@@ -3,6 +3,7 @@ import cv2
 import copy
 from collections import defaultdict
 
+
 class Pose(object):
     POSE_KEYPOINTS = 18
     FACE_KEYPOINTS = 70
@@ -28,12 +29,20 @@ class Pose(object):
     LEar = 17
     Background = 18
 
+    # https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/7325aa32dce312539e7414c1ba599631c3ad221b/include/openpose/pose/poseParametersRender.hpp
+    DRAW_PAIRS = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8],
+                  [8, 9], [9, 10], [1, 11], [11, 12], [12, 13], [1, 0],
+                  [0, 14], [14, 16], [0, 15], [15, 17]]
+
+    DRAW_COLORS = [[255, 0, 85], [255, 0, 0], [255, 85, 0], [255, 170, 0], [
+        255, 255, 0
+    ], [170, 255, 0], [85, 255, 0], [0, 255, 0], [0, 255, 85], [0, 255, 170],
+                   [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255],
+                   [255, 0, 170], [170, 0, 255], [255, 0, 255], [85, 0, 255]]
+
     def __init__(self):
-        self.keypoints = np.zeros(
-            (Pose.POSE_KEYPOINTS +
-             Pose.FACE_KEYPOINTS +
-             Pose.HAND_KEYPOINTS * 2,
-             3))
+        self.keypoints = np.zeros((Pose.POSE_KEYPOINTS + Pose.FACE_KEYPOINTS +
+                                   Pose.HAND_KEYPOINTS * 2, 3))
 
     def _format_keypoints(self):
         return self.keypoints
@@ -44,11 +53,12 @@ class Pose(object):
 
     def face_keypoints(self):
         kp = self._format_keypoints()
-        return kp[self.POSE_KEYPOINTS:(self.POSE_KEYPOINTS+self.FACE_KEYPOINTS), :]
+        return kp[self.POSE_KEYPOINTS:(
+            self.POSE_KEYPOINTS + self.FACE_KEYPOINTS), :]
 
     def hand_keypoints(self):
         kp = self._format_keypoints()
-        base = kp[self.POSE_KEYPOINTS+self.FACE_KEYPOINTS:, :]
+        base = kp[self.POSE_KEYPOINTS + self.FACE_KEYPOINTS:, :]
         return [base[:self.HAND_KEYPOINTS, :], base[self.HAND_KEYPOINTS:, :]]
 
     def face_bbox(self):
@@ -68,12 +78,25 @@ class Pose(object):
         score = min(p[16, 2], p[17, 2], p[0, 2])
         return [(xmin, ymin), (xmax, ymax), score]
 
+    def draw(self, img, thickness=5):
+        def to_pt(i):
+            return (int(self.keypoints[i, 0] * img.shape[1]),
+                    int(self.keypoints[i, 1] * img.shape[0]))
+
+        draw_threshold = 0.35
+        for ([a, b], color) in zip(self.DRAW_PAIRS, self.DRAW_COLORS):
+            if self.keypoints[a, 2] > draw_threshold and \
+               self.keypoints[b, 2] > draw_threshold:
+                cv2.line(img, to_pt(a), to_pt(b), color, thickness)
+
+        return img
+
     @staticmethod
     def from_buffer(keypoints_buffer):
         pose = Pose()
         shape = pose.keypoints.shape
-        pose.keypoints = (
-            np.frombuffer(keypoints_buffer, dtype=np.float32).reshape(shape))
+        pose.keypoints = (np.frombuffer(keypoints_buffer,
+                                        dtype=np.float32).reshape(shape))
         return pose
 
 
@@ -93,8 +116,8 @@ def nms(orig_poses, overlapThresh):
 
     max_boxes = len(poses)
     joints_4d = np.stack(poses, axis=2)
-    pose_scores = np.sum(joints_4d[:,2,:], axis=0)
-    num_joints_per_pose = np.sum(joints_4d[:,2,:] > 0.2, axis=0)
+    pose_scores = np.sum(joints_4d[:, 2, :], axis=0)
+    num_joints_per_pose = np.sum(joints_4d[:, 2, :] > 0.2, axis=0)
     # sort by score
     idxs = np.argsort(pose_scores)
     idxs_orig = np.argsort(pose_scores)
@@ -105,9 +128,9 @@ def nms(orig_poses, overlapThresh):
     for i, idx in enumerate(idxs):
         pose = poses[idx]
         for pi in range(num_joints):
-            if pose[pi,2] > 0.2:
-                x_pos = int(pose[pi,1] - (pose[pi,1] % overlapThresh))
-                y_pos = int(pose[pi,0] - (pose[pi,0] % overlapThresh))
+            if pose[pi, 2] > 0.2:
+                x_pos = int(pose[pi, 1] - (pose[pi, 1] % overlapThresh))
+                y_pos = int(pose[pi, 0] - (pose[pi, 0] % overlapThresh))
                 for xp in range(x_pos - 1, x_pos + 2):
                     x_buckets[pi][xp].add(idx)
                 for yp in range(y_pos - 1, y_pos + 2):
@@ -128,9 +151,9 @@ def nms(orig_poses, overlapThresh):
         overlaps = defaultdict(int)
         pose = poses[i]
         for pi in range(num_joints):
-            if pose[pi,2] > 0.2:
-                x_pos = int(pose[pi,1] - (pose[pi,1] % overlapThresh))
-                y_pos = int(pose[pi,0] - (pose[pi,0] % overlapThresh))
+            if pose[pi, 2] > 0.2:
+                x_pos = int(pose[pi, 1] - (pose[pi, 1] % overlapThresh))
+                y_pos = int(pose[pi, 0] - (pose[pi, 0] % overlapThresh))
 
                 x_set = set()
                 for xp in range(x_pos - 1, x_pos + 2):
@@ -152,8 +175,7 @@ def nms(orig_poses, overlapThresh):
                 duplicates.append(ii)
 
         # delete all indexes from the index list that have
-        idxs = np.delete(idxs, np.concatenate(
-            ([last], np.array(duplicates))))
+        idxs = np.delete(idxs, np.concatenate(([last], np.array(duplicates))))
 
     # return only the bounding boxes that were picked
     out_poses = []
