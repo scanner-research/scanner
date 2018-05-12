a11a3a6aa4bbff86ab0d7231895e06e34a2b8725
Alex Poms <alexpoms@gmail.com>
2018-05-11 18:30:17 -0700

    Add 05_sources_sinks tutorial

--- examples/util.py
+++ examples/util.py
@@ -11,6 +11,8 @@ except ImportError:
 VID_URL = "https://storage.googleapis.com/scanner-data/public/sample-clip.mp4"
 VID_PATH = '/tmp/example.mp4'
 
+IMG_PATH = '/tmp/example.mp4'
+
 
 def download_video():
     if not os.path.isfile(VID_PATH):
@@ -21,3 +23,17 @@ def download_video():
                 f.write(block)
             f.flush()
     return VID_PATH
+
+
+def download_images():
+    img_template = (
+        'https://storage.googleapis.com/scanner-data/public/sample-frame-{:d}.jpg')
+    output_template = 'sample-frame-{:d}.jpg'
+
+    for i in range(1, 4):
+        with open(output_template.format(i), 'wb') as f:
+            resp = requests.get(img_template.format(i), stream=True)
+            assert resp.ok
+            for block in resp.iter_content(1024):
+                f.write(block)
+            f.flush()
