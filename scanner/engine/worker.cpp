500d0d81cf505739cb2bc955e0ac6720f7645a9b
Will Crichton <wcrichto@cs.stanford.edu>
2018-05-13 00:46:53 +0000

    Revert API changes

--- scanner/engine/worker.cpp
+++ scanner/engine/worker.cpp
@@ -1641,10 +1641,10 @@ bool WorkerImpl::process_job(const proto::BulkJobParameters* job_params,
     }
     // We batch up retired tasks to avoid sync overhead
     std::vector<std::tuple<i32, i64, i64>> batched_retired_tasks;
-    while (retired_tasks.size_approx() > 0) {
+    while (retired_tasks.size() > 0) {
       // Pull retired tasks
       std::tuple<i32, i64, i64> task_retired;
-      retired_tasks.wait_dequeue(task_retired);
+      retired_tasks.pop(task_retired);
       batched_retired_tasks.push_back(task_retired);
     }
     if (!batched_retired_tasks.empty()) {
