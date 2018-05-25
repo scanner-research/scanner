37f525e642bab9c18bc1ad388f6919c4d20b8e90
Alex Poms <alexpoms@gmail.com>
2018-05-24 18:19:00 -0700

    Bump python package version

--- python/setup.py
+++ python/setup.py
@@ -119,7 +119,7 @@ if platform == 'linux' or platform == 'linux2':
 
 setup(
     name='scannerpy',
-    version='0.0.2',
+    version='0.0.3',
     description='Efficient video analysis at scale',
     long_description='',
     url='https://github.com/scanner-research/scanner',
