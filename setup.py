efa4115fb3fbb916e781b769ab3f486e18772ac7
Alex Poms <alexpoms@gmail.com>
2018-05-28 22:21:00 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ module1 = Extension(
 
 setup(
     name='scannerpy',
-    version='0.2.4',
+    version='0.2.5',
     description='Efficient video analysis at scale',
     long_description='',
     url='https://github.com/scanner-research/scanner',
