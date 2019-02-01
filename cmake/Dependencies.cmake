d8c4c0d2f6261a1f6f79407f5a758d80b9eab9d7
Alex Poms <alexpoms@gmail.com>
2019-02-01 11:35:45 -0800

    Make hwang a possible optional dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -45,6 +45,12 @@ if (NO_FFMPEG STREQUAL "false")
   add_definitions(-DHAVE_FFMPEG)
 endif()
 
+if (NO_HWANG STREQUAL "false")
+  find_package(Hwang REQUIRED)
+  include_directories("${HWANG_INCLUDE_DIRS}")
+  add_definitions(-DHAVE_HWANG)
+endif()
+
 ###### Required Dependencies #######
 find_package(SaneProtobuf REQUIRED)
 find_package(GRPC REQUIRED)
@@ -62,7 +68,6 @@ find_package(Iconv REQUIRED)
 find_package(Storehouse REQUIRED CONFIG
   PATHS "${CMAKE_SOURCE_DIR}/thirdparty/install"
   "${STOREHOUSE_DIR}")
-find_package(Hwang REQUIRED)
 find_package(TinyToml REQUIRED)
 find_package(OpenCV COMPONENTS ${OPENCV_DESIRED_COMPONENTS})
 
@@ -101,7 +106,6 @@ set(SCANNER_LIBRARIES
 include_directories(
   "."
   "${CMAKE_CURRENT_BINARY_DIR}" # for protobuf generated files
-  "${HWANG_INCLUDE_DIRS}"
   "${PROTOBUF_INCLUDE_DIRS}"
   "${GRPC_INCLUDE_DIRS}"
   "${TINYTOML_INCLUDE_DIR}"
