e9cf4f992335d8ac37c1e86927189b1cdbe8be2f
Will Crichton <wcrichto@cs.stanford.edu>
2019-03-18 18:25:53 +0000

    Fix quickstart

--- examples/apps/quickstart/main.py
+++ examples/apps/quickstart/main.py
@@ -1,23 +1,24 @@
-from scannerpy import Client
+from scannerpy import Client, PerfParams
+from scannerpy.storage import NamedVideoStream
 
 def main():
     sc = Client()
-    
+
     # Create a stored stream to represent the input video
     input_stream = NamedVideoStream(sc, 'sample-clip', path='sample-clip.mp4')
-    
+
     # Define a Computation Graph
     frame = sc.io.Input([input_stream])
     sampled_frame = sc.streams.Stride(frame, 3) # Select every third frame
     resized = sc.ops.Resize(frame=sampled_frame, width=640, height=480) # Resize input frame
-    
+
     # Create a stored stream to represent the output video
     output_stream = NamedVideoStream(sc, 'sample-clip-resized')
     output = sc.io.Output(resized, [output_stream])
-    
-    # Execute the computation graph 
-    sc.run(output)
-    
+
+    # Execute the computation graph
+    sc.run(output, PerfParams.estimate())
+
     # Save the resized video as an mp4 file
     output_stream.save_mp4('sample-clip-resized')
 
