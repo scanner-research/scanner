5e7f56383f7c2f2f502bb899f09fb8ddd45de7ed
Will Crichton <wcrichto@cs.stanford.edu>
2019-03-27 03:07:53 +0000

    Move scannertols install into tutorials tests

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -28,14 +28,14 @@ test_docker() {
     if [[ "$TEST_TYPE" = "cpp" ]]; then
         TEST_COMMAND="cd /opt/scanner/build && CTEST_OUTPUT_ON_FAILURE=1 make test ARGS='-V -E PythonTests'"
     elif [[ "$TEST_TYPE" = "tutorials" ]]; then
-        TEST_COMMAND="cd /opt/scanner/ && python3 setup.py test --addopts '-k test_tutorial'"
+        TEST_COMMAND="$INSTALL_SCANNERTOOLS && cd /opt/scanner/ && python3 setup.py test --addopts '-k test_tutorial'"
     elif [[ "$TEST_TYPE" = "integration" ]]; then
         TEST_COMMAND="cd /opt/scanner/ && python3 setup.py test --addopts '-k \\\"not test_tutorial\\\"'"
     fi
     # We add -local to make sure it doesn't run the remote image if the build fails.
     docker pull $DOCKER_TEST_REPO:$1-$TRAVIS_BUILD_NUMBER
     docker run $DOCKER_TEST_REPO:$1-$TRAVIS_BUILD_NUMBER /bin/bash \
-           -c "adduser --disabled-password --gecos \"\" user && (yes | pip3 uninstall grpcio protobuf) && chmod -R 777 /opt/scanner && su -c \"cd /opt/scanner/dist && (yes | pip3 install --user *) && $INSTALL_SCANNERTOOLS && $TEST_COMMAND\" user"
+           -c "adduser --disabled-password --gecos \"\" user && (yes | pip3 uninstall grpcio protobuf) && chmod -R 777 /opt/scanner && su -c \"cd /opt/scanner/dist && (yes | pip3 install --user *) && $TEST_COMMAND\" user"
     docker rm $(docker ps -a -f status=exited -q)
     docker rmi -f $DOCKER_REPO:$1-$TRAVIS_BUILD_NUMBER
 }
