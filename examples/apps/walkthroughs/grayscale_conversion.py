ecebbda255af0fee9efa5021d6c3eb3737dd441b
Alex Poms <alexpoms@gmail.com>
2019-03-27 22:51:33 -0700

    [docs] misc updates, delete unused apps

--- examples/apps/walkthroughs/grayscale_conversion.py
+++ examples/apps/walkthroughs/grayscale_conversion.py
@@ -14,20 +14,20 @@ def main():
 
     # Define a Computation Graph
     frames = cl.io.Input([input_stream])
-    sampled_frames = cl.streams.Stride(frames, [2]) # Select every third frame
+    sampled_frames = cl.streams.Stride(frames, [2]) # Select every other frame
     resized_frames = cl.ops.Resize(frame=sampled_frames, width=[640], height=[480]) # Resize input frame
     grayscale_frames = cl.ops.ConvertColor(frame=resized_frames, conversion=['COLOR_RGB2GRAY']) 
     grayscale3_frames = cl.ops.CloneChannels(frame=grayscale_frames, replications=3) 
 
     # Create a stored stream to represent the output video
-    output_stream = sp.NamedVideoStream(cl, 'sample-clip-grayscale')
+    output_stream = sp.NamedVideoStream(cl, 'sample-grayscale')
     output = cl.io.Output(grayscale3_frames, [output_stream])
 
     # Execute the computation graph
     cl.run(output, sp.PerfParams.manual(50, 250))
 
     # Save the resized video as an mp4 file
-    output_stream.save_mp4('sample-clip-grayscale')
+    output_stream.save_mp4('sample-grayscale')
 
     input_stream.delete(cl)
     output_stream.delete(cl)
