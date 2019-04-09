9c4183aee37c52d6ef56123206eb6751ad26a305
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-09 20:40:28 +0000

    Move sample video into sp.util

--- python/scannerpy/util.py
+++ python/scannerpy/util.py
@@ -35,3 +35,50 @@ def default(d, k, v):
     if k not in d:
         return v() if callable(v) else v
     return d[k]
+
+
+VID_URL = "https://storage.googleapis.com/scanner-data/public/sample-clip.mp4"
+VID_PATH = '/tmp/example.mp4'
+
+IMG_PATH = '/tmp/example.mp4'
+
+
+def download_video():
+    try:
+        import requests
+    except ImportError:
+        print(
+            'You need to install requests to run this. Try running:\npip3 install requests'
+        )
+        exit()
+
+    if not os.path.isfile(VID_PATH):
+        with open(VID_PATH, 'wb') as f:
+            resp = requests.get(VID_URL, stream=True)
+            assert resp.ok
+            for block in resp.iter_content(1024):
+                f.write(block)
+            f.flush()
+    return VID_PATH
+
+
+def download_images():
+    try:
+        import requests
+    except ImportError:
+        print(
+            'You need to install requests to run this. Try running:\npip3 install requests'
+        )
+        exit()
+
+    img_template = (
+        'https://storage.googleapis.com/scanner-data/public/sample-frame-{:d}.jpg')
+    output_template = 'sample-frame-{:d}.jpg'
+
+    for i in range(1, 4):
+        with open(output_template.format(i), 'wb') as f:
+            resp = requests.get(img_template.format(i), stream=True)
+            assert resp.ok
+            for block in resp.iter_content(1024):
+                f.write(block)
+            f.flush()
