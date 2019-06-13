67b8972b8b6b025334f316dca9583fdcf5f753e4
Will Crichton <wcrichto@cs.stanford.edu>
2019-06-13 21:27:20 +0000

    Fix worker duplicate issue in kubernetes tutorial

--- examples/apps/gcp_kubernetes/master.py
+++ examples/apps/gcp_kubernetes/master.py
@@ -1,8 +1,8 @@
 import scannerpy
 import os
 
-scannerpy.start_master(
-    port='8080',
-    block=True,
-    watchdog=False,
-    no_workers_timeout=180)
+if __name__ == "__main__":
+    scannerpy.start_master(port='8080',
+                           block=True,
+                           watchdog=False,
+                           no_workers_timeout=180)
