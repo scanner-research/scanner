9079a2d91d1f87752a9a93d1aa580dfbf1870188
Pablo Munoz <jpablomch@gmail.com>
2020-02-02 22:01:35 +0000

    Add flags for OS in deps_openvino.sh

--- deps_openvino.sh
+++ deps_openvino.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-OPENVINO_REQ_PKGS=(
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    OPENVINO_REQ_PKGS=(
         libusb-1.0-0-dev
         libgstreamer1.0-0
         gstreamer1.0-plugins-base
         gstreamer1.0-plugins-good
         gstreamer1.0-plugins-bad
     )
-apt update
-apt install -y ${OPENVINO_REQ_PKGS[@]}
+    apt update
+    apt install -y ${OPENVINO_REQ_PKGS[@]}
+        # ...
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Support for Scanner with OpenVINO in Mac OSX is not available at this time." 
+    exit 1  
+        # Mac OSX
+else
+    # Unknown.
+    echo "Unknown OSTYPE: $OSTYPE. Exiting."
+    exit 1
+fi
+
